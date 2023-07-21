88157d4b12fcad7bf8ccf98892fb2486930f297a
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2023-07-21 13:56:32 +0200

    Display function to command line.

---
+++ +casos/+package/+functions/@FunctionWrapper/disp.m
@@ -0,0 +1,13 @@
+function disp(obj)
+% Display function signature.
+
+args_i = cellfun(@get_signature, struct2cell(obj.arg_i), 'UniformOutput', false);
+args_o = cellfun(@get_signature, struct2cell(obj.arg_o), 'UniformOutput', false);
+
+% separate arguments by comma
+args_i(2,1:end-1) = {','};
+args_o(2,1:end-1) = {','};
+
+fprintf('%s:(%s)->(%s) %s\n', obj.name, [args_i{:}], [args_o{:}], obj.class_name);
+
+end
