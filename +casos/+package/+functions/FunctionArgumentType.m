48296146fb69e4bece188dd1d293650e219193fc
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2023-08-08 13:52:11 +0200

    Extended functions: Check and create arguments of type.

--- +casos/+package/+functions/FunctionArgumentType.m
+++ +casos/+package/+functions/FunctionArgumentType.m
@@ -1,8 +1,33 @@
 classdef FunctionArgumentType
 % Possible types of arguments for functions.
 
+    properties
+        classname;
+        construct;
+    end
+
     enumeration
-        DM, SX, MX, PS
+        DM ('casadi.DM', @casos.package.functions.FunctionDMArgument)
+        SX ('casadi.SX', @casos.package.functions.FunctionSXArgument)
+        MX ('casadi.MX', @casos.package.functions.FunctionMXArgument) 
+        PS ('casos.PS', @casos.package.functions.FunctionPSArgument)
+    end
+
+    methods
+        function type = FunctionArgumentType(cls,cst)
+            % Create new function argument type.
+            type.classname = cls;
+            type.construct = cst;
+        end
+
+        function tf = isOfType(type,var)
+            % Check if variable is of given type.
+            tf = isa(var, type.classname);
+        end
+
+        function arg = newArgument(type,varargin)
+            % Create new function argument of given type.
+            arg = feval(type.construct, varargin{:});
+        end
     end
 end
-
