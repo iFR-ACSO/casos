50473c0d1b970c0eb0811d2ad228f79ae7b495ab
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2023-09-13 14:45:59 +0200

    Improved SeDuMi interface.

--- +casos/+package/+solvers/@SedumiInterface/buildproblem.m
+++ +casos/+package/+solvers/@SedumiInterface/buildproblem.m
@@ -68,32 +68,36 @@ Na.q = (obj.getdimc(Ka,'q'));
 Na.r = (obj.getdimc(Ka,'r'));
 Na.s = (obj.getdimc(Ka,'s'));
 
-% rewrite linear and state constraints into
+% define xl = zl + lbx, xc = zc + cbx, 
+% where zl is a nonnegative variable and zc is element of Kx
+zb = [lbx; cbx];
+% rewrite linear and state constraints, that is,
 %
-%   a*x + sua = uba
-%   a*x - sla = lba
-%   a*x - sca = cba
-%     x + sux = ubx
-%     x - slx = lbx
-%     x - scx = cbx
+%   lba <= al*x <= uba, ac*x in Ka
+%   lbx <=  xl  <= ubx,  xc  in Kx
 %
-% where sla, sua, slx, sux are nonnegative slack variables and sca, scx are
-% elements of Ka and Kx, respectively.
+% with a = [al; ac], x = (xl, xc)
+% into equality constraints
+%
+%   al*x + sua = uba <-> al*z + sua = uba - al*zb
+%   al*x - sla = lba <-> al*z - sla = lba - al*zb
+%   ac*x - sca = cba <-> ac*z - sca = cba - ac*zb
+%     xl + sux = ubx <->   zl + sux = ubx -   lbx
+%
+% where sla, sua, and sux are nonnegative slack variables 
+% and sca is element of Ka
 Nx_c = n - Nx.l;
 Na_c = m - Na.l;
 
-A = [ [
-        a(1:Na.l,:)
-        a
-        speye(Nx.l,n)
-        speye(n)
-    ] blkdiag(+speye(Na.l), -speye(m), +speye(Nx.l), -speye(n))
-];
-b = [uba; lba; cba; ubx; lbx; cbx];
-c = [g; sparse(m+Na.l+n+Nx.l,1)];
+% [al; al; ac]
+A0 = [a(1:Na.l,:); a];
+
+% min c'*(z,s) s.t. A*(z,s) = b
+A = [ [A0; speye(Nx.l,n)] blkdiag(+speye(Na.l), -speye(m), +speye(Nx.l)) ];
+b = [ [uba; lba; cba] - A0*zb; ubx - lbx ];
+c = [g; sparse(m+Na.l+Nx.l,1)];
 
 % set cone for SeDuMi
-K.f = n;
 K.l = 2*(Na.l + Nx.l);
 K.q = [Na.q Nx.q];
 K.r = [Na.r Nx.r];
@@ -101,11 +105,15 @@ K.s = [Na.s Nx.s];
 
 obj.cone = K;
 
-% reorder slack variables to (l,q,r,s)
+% reorder decision variables (z,s) to (l,q,r,s)
+% current order: (z,s) = [zl zc sua sla sca sux]
+Iz = [ones(1,Nx.l) 2*ones(1,sum(Nx.q)) 3*ones(1,sum(Nx.r)) 4*ones(1,sum(Nx.s.^2))];
 Ia = [ones(1,2*Na.l) 2*ones(1,sum(Na.q)) 3*ones(1,sum(Na.r)) 4*ones(1,sum(Na.s.^2))];
-Ix = [ones(1,2*Nx.l) 2*ones(1,sum(Nx.q)) 3*ones(1,sum(Nx.r)) 4*ones(1,sum(Nx.s.^2))];
+Ix =  ones(1,Nx.l);
 
-[~,idx] = sort([zeros(1,n) Ia Ix]);
+% new order: [zl sua sla sux | zc_q sca_q zc_r sca_r zc_s sca_s] 
+% (see https://de.mathworks.com/help/matlab/ref/sort.html#bt8nojg-1-I)
+[~,idx] = sort([Iz Ia Ix]);
 
 % return SeDuMi structures A,b,c
 obj.fhan = casadi.Function('f',struct2cell(obj.args_in),{A(:,idx) b c(idx)},fieldnames(obj.args_in),{'A' 'b' 'c'});
@@ -115,15 +123,21 @@ X = casadi.MX.sym('x',size(c));
 Y = casadi.MX.sym('y',size(b));
 S = c - A'*Y;
 
-% dual variables corresponding to constraints and variables
-Sc = mat2cell(S(n+1:end),[Na.l Na.l Nx.l Nx.l Na_c Nx_c],1);
-% multipliers for interval constraints
-lam_a_l = Sc{1} - Sc{2};
-lam_x_l = Sc{3} - Sc{4};
-% multipliers for cone constraints
-lam_a_c = -Sc{5};
-lam_x_c = -Sc{6};
+% primal decision variables in the NEW order
+Xc = mat2cell(X,[Nx.l Na.l Na.l Nx.l sum(Nx.q) sum(Na.q) sum(Nx.r) sum(Na.r) sum(Nx.s.^2) sum(Na.s.^2)],1);
+x = [Xc{1}; Xc{5}; Xc{7}; Xc{9}] + zb;
 
-obj.ghan = casadi.Function('g',[struct2cell(obj.args_in)' {X Y}],{X(1:n) (c'*X) [lam_a_l; lam_a_c] [lam_x_l; lam_x_c]},[fieldnames(obj.args_in)' {'X' 'Y'}],obj.names_out);
+% dual variables corresponding to constraints and variables
+% S in the ORIGINAL order of (z,s), 
+% that is, S = [y_lbx y_cbx y_uba y_lba y_cba y_ubx]
+Sc = mat2cell(S,[Nx.l Nx_c Na.l Na.l Na_c Nx.l],1);
+% multipliers for interval constraints
+lam_a_l = Sc{3} - Sc{4};    % y_uba - y_lba
+lam_x_l = Sc{6} - Sc{1};    % y_ubx - y_lbx
+% multipliers for cone constraints
+lam_a_c = -Sc{5};  %  -y_cba
+lam_x_c = -Sc{2};  %  -y_cbx
+
+obj.ghan = casadi.Function('g',[struct2cell(obj.args_in)' {X Y}],{x (g'*x) [lam_a_l; lam_a_c] [lam_x_l; lam_x_c]},[fieldnames(obj.args_in)' {'X' 'Y'}],obj.names_out);
 
 end
--- +casos/+package/+solvers/@SedumiInterface/eval.m
+++ +casos/+package/+solvers/@SedumiInterface/eval.m
@@ -1,6 +1,26 @@
 function argout = eval(obj,argin)
 % Call SeDuMi interface.
 
+% pre-process bounds
+lba = sparse(argin{4});
+uba = sparse(argin{5});
+cba = sparse(argin{6});
+lbx = sparse(argin{7});
+ubx = sparse(argin{8});
+cbx = sparse(argin{9});
+
+% dimensions of original problem
+nl = length(lbx);
+nc = length(cbx);
+ml = length(lba);
+mc = length(cba);
+
+% detect infinite lower variable bounds
+If = find(isinf(lbx));
+% prepare for infinite lower variable bounds
+argin{7}(If) = 0;
+
+% evaluate problem structure
 prob = call(obj.fhan,argin);
 
 % to double
@@ -13,17 +33,55 @@ K = obj.cone;
 % options to SeDuMi
 opts = obj.sdpopt.solveroptions;
 
+% reorder decision variables
+idx = [If setdiff(1:length(c),If)];
+
+% remove trivial constraints
+I = false(size(b));
+J = false(size(c));
+
+% detect equality constraints
+Ila = find(lba == uba);
+% remove lower bound constraints
+I(Ila) = true;
+% remove slack variables (sua,sla)
+J(nl+[Ila ml+Ila]) = true;
+
+% detect constant variables
+Ilx = find(lbx == ubx);
+% remove slack variable sux
+% (this leaves zl(i) = 0)
+J(nl+2*ml+Ilx) = true;
+
 % detect infinite bounds
-I = isinf(b);
-% remove infinite interval constraints
+Iba = find(isinf([uba;lba]));
+Ibx = find(isinf(ubx));
+% remove infinite bound constraints
+I(Iba) = true;
+I(2*ml+mc+Ibx) = true;
+% remove slack variables (sua,sla,sux)
+J(nl+[Iba 2*ml+Ibx]) = true;
+
+% purge constraints
 A(I,:) = [];
 b(I)   = [];
 
-% pre-init dual solution
-y = spalloc(length(I),1,nnz(~I));
+% purge variables
+idx(J) = [];
+
+A = A(:,idx);
+c = c(idx);
+
+% modify cone
+K.f = length(If);
+K.l = K.l - nnz(J) - length(If);
 
 % call SeDuMi
-[x,y(~I),info] = sedumi(A,b,c,K,opts);
+[x_,y_,info] = sedumi(A,b,full(c),K,opts);
+
+% assign full solution
+x = sparse(idx,1,x_,length(J),1);
+y = sparse(find(~I),1,y_,length(I),1);
 
 if ~obj.sdpopt.error_on_fail
     % continue regardless of feasibility
