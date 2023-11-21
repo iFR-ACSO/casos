79df5c7b85a148258132a589c763fc9ba858ab76
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2023-11-21 15:21:15 +0100

    Some SOS demo.

---
+++ demo/polyopt.m
@@ -0,0 +1,21 @@
+% Polynomial optimization.
+
+% indeterminate variable
+x = casos.PS('x');
+% some polynomial
+f = x^4 + 10*x;
+% scalar decision variable
+g = casos.PS.sym('g');
+
+% define SOS problem:
+%   min g s.t. (f + g) is SOS
+sos = struct('x',g,'f',g,'g',f+g);
+% constraint is scalar SOS cone
+opts = struct('Kg',struct('s',1));
+
+% solve by relaxation to SDP
+S = casos.sossol('S','sedumi',sos,opts);
+% evaluate
+sol = S();
+
+fprintf('Minimum is %g.\n', double(sol.f))
