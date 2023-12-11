b8272458e6c1928382658604c40bc99d9fc3d9c8
JOlCode <61011693+JOlCode@users.noreply.github.com>
2023-12-11 14:45:54 +0100

    add VDP VS-iteration example

---
+++ demo/roaest2.m
@@ -0,0 +1,162 @@
+% Validate stability with LF candidate.
+clear
+clc
+
+% system states
+x = casos.PS('x',2,1);
+
+% system dynamics
+f = [-x(2); x(1) + (x(1)^2 - 1)*x(2)];
+
+% Lyapunov function candidate
+Vval = 1.5*x(1)^2 - x(1)*x(2) + x(2)^2;
+p = Vval*5;
+
+% derivative w.r.t. time
+Vvaldot = nabla(Vval,x)*f;
+
+% Lyapunov function candidate
+V = casos.PS.sym('v',monomials(x,2));
+
+% SOS multiplier
+s1 = casos.PS.sym('s1',monomials(x,0:2),'gram');
+s2 = casos.PS.sym('s2',monomials(x,0:2),'gram');
+
+% enforce positivity
+l = 1e-6*(x'*x);
+
+% level of stability
+g = casos.PS.sym('g');
+b = casos.PS.sym('b');
+
+% ignore infeasibility
+opts.error_on_fail = false;
+
+%% setup solver
+
+% solver 1: gamma-step
+sos1 = struct('x',s1,'g',[ s1*(V-g)-nabla(V,x)*f-l],'p',[g;V]);
+
+% states + constraint are SOS cones
+opts.Kx.s = 1; 
+opts.Kc.s = 1;
+
+S1 = casos.sossol('S','sedumi',sos1,opts);
+
+% solver 2: beta-step
+sos2 = struct('x',s2,'g', s2*(p-b)+g-V,'p',[g;b;V]);
+
+% states + constraint are SOS cones
+opts.Kx.s = 1; 
+opts.Kc.s = 1;
+
+S2 = casos.sossol('S','sedumi',sos2,opts);
+
+% solver 3: V-step
+[~,R,~] =poly2basis(s1);
+s1_sym = casos.PS.sym('s1',R);
+[~,R,~] =poly2basis(s2);
+s2_sym = casos.PS.sym('s2',R);
+
+% s1 = casos.PS.sym()
+Vlb = casos.PS(monomials(x,1:4),-inf(14,1));
+Vub = casos.PS(monomials(x,1:4),+inf(14,1));
+
+sos3 = struct('x',V,...
+              'g',[V-l; s2_sym*(p-b)+g-V;s1_sym*(V-g)-nabla(V,x)*f-l],...
+              'p',[b,g,s1_sym,s2_sym]);
+
+opts.Kx.s = 0; 
+opts.Kx.l = 1; 
+opts.Kc.s = 3;
+
+S3 = casos.sossol('S','sedumi',sos3,opts);
+
+%% gamma-beta-V-iteration
+
+tic 
+profile on
+for iter = 1:10
+
+% gamma step: find largest possible stable level set
+lb = 0; 
+ub = 1000;
+
+% bisection
+while (ub - lb > 1e-1)
+    s1val = [];
+    s2val = [];
+    ptry = (lb+ub)/2;
+
+    % evaluate parametrized SOS problem
+    sol1 = S1('p',[ptry;Vval]);
+
+    switch (S1.stats.UNIFIED_RETURN_STATUS)
+        case 'SOLVER_RET_SUCCESS', ...
+            s1val = sol1.x;
+            lb = ptry;
+        case {'SOLVER_RET_INFEASIBLE' 'SOLVER_RET_NAN'}, ...
+                ub = ptry;
+        otherwise, error('Failed.')
+    end
+end
+
+if ~isempty(s1val)
+    fprintf('Maximal stable level set is %g.\n', lb);
+else
+    disp('Not feasible in gamma step!')
+    break
+end
+gval = lb;
+
+
+
+% beta step: find largest possible stable level set
+lb = 0; 
+ub = 1000;
+
+% bisection
+while (ub - lb > 1e-1)
+    ptry = (lb+ub)/2;
+
+    % evaluate parametrized SOS problem
+    sol2 = S2('p',[gval;ptry;Vval]);
+
+    switch (S2.stats.UNIFIED_RETURN_STATUS)
+        case 'SOLVER_RET_SUCCESS', ...
+            lb = ptry;
+            s2val = sol2.x;
+        case {'SOLVER_RET_INFEASIBLE' 'SOLVER_RET_NAN'}, ...
+            ub = ptry;
+        otherwise, error('Failed.')
+    end
+end
+if ~isempty(s2val)
+    fprintf('Maximal shape function level set is %g.\n', lb);
+else
+    disp('Not feasible in beta step!')
+    break
+end
+
+bval = lb;
+
+
+
+ % V-step
+ sol3 = S3('p',[bval,gval,s1val,s2val],'lbx',Vlb,'ubx',Vub);
+
+
+    switch (S3.stats.UNIFIED_RETURN_STATUS)
+        case 'SOLVER_RET_SUCCESS',... 
+                Vval = sol3.x;
+
+        case {'SOLVER_RET_INFEASIBLE' 'SOLVER_RET_NAN'}
+            disp('V step infeasible')
+            break
+        otherwise, error('Failed.')
+    end
+end
+profile viewer
+toc
+
+
