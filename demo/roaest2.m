0720b17d08469c3032a7d307c778c390b8626fdc
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2023-12-14 14:42:31 +0100

    Improved readability of demo.

--- demo/roaest2.m
+++ demo/roaest2.m
@@ -1,6 +1,4 @@
-% Validate stability with LF candidate.
-clear
-clc
+% Estimate region of attraction by V-s-iteration.
 
 % system states
 x = casos.PS('x',2,1);
@@ -11,9 +9,6 @@ f = [-x(2); x(1) + (x(1)^2 - 1)*x(2)];
 % Lyapunov function candidate
 Vval = 1.5*x(1)^2 - x(1)*x(2) + x(2)^2;
 p = Vval*5;
-
-% derivative w.r.t. time
-Vvaldot = nabla(Vval,x)*f;
 
 % Lyapunov function candidate
 V = casos.PS.sym('v',monomials(x,2));
@@ -30,52 +25,48 @@ g = casos.PS.sym('g');
 b = casos.PS.sym('b');
 
 % ignore infeasibility
-opts.error_on_fail = false;
+opts = struct('error_on_fail', false);
 
 %% setup solver
 
 % solver 1: gamma-step
-sos1 = struct('x',s1,'g',[ s1*(V-g)-nabla(V,x)*f-l],'p',[g;V]);
+sos1 = struct('x',s1,'p',[g;V]);
+sos1.('g') = s1*(V-g)-nabla(V,x)*f-l;
 
 % states + constraint are SOS cones
-opts.Kx.s = 1; 
-opts.Kc.s = 1;
+opts.Kx = struct('s', 1);
+opts.Kc = struct('s', 1);
 
 S1 = casos.sossol('S','sedumi',sos1,opts);
 
 % solver 2: beta-step
-sos2 = struct('x',s2,'g', s2*(p-b)+g-V,'p',[g;b;V]);
+sos2 = struct('x',s2,'p',[g;b;V]);
+sos2.('g') = s2*(p-b)+g-V;
 
 % states + constraint are SOS cones
-opts.Kx.s = 1; 
-opts.Kc.s = 1;
+opts.Kx = struct('s', 1);
+opts.Kc = struct('s', 1);
 
 S2 = casos.sossol('S','sedumi',sos2,opts);
 
 % solver 3: V-step
-[~,R,~] =poly2basis(s1);
-s1_sym = casos.PS.sym('s1',R);
-[~,R,~] =poly2basis(s2);
-s2_sym = casos.PS.sym('s2',R);
+s1_sym = casos.PS.sym('s1',basis(s1));
+s2_sym = casos.PS.sym('s2',basis(s2));
 
 % s1 = casos.PS.sym()
-Vlb = casos.PS(monomials(x,1:4),-inf(14,1));
-Vub = casos.PS(monomials(x,1:4),+inf(14,1));
+Vlb = casos.PS(basis(V),-inf);
+Vub = casos.PS(basis(V),+inf);
 
-sos3 = struct('x',V,...
-              'g',[V-l; s2_sym*(p-b)+g-V;s1_sym*(V-g)-nabla(V,x)*f-l],...
-              'p',[b,g,s1_sym,s2_sym]);
+sos3 = struct('x',V,'p',[b,g,s1_sym,s2_sym]);
+sos3.('g') = [V-l; s2_sym*(p-b)+g-V; s1_sym*(V-g)-nabla(V,x)*f-l];
 
-opts.Kx.s = 0; 
-opts.Kx.l = 1; 
-opts.Kc.s = 3;
+opts.Kx = struct('s', 0, 'l', 1); 
+opts.Kc = struct('s', 3);
 
 S3 = casos.sossol('S','sedumi',sos3,opts);
 
-%% gamma-beta-V-iteration
+%% V-s-iteration
 
-tic 
-profile on
 for iter = 1:10
 
 % gamma step: find largest possible stable level set
@@ -86,17 +77,17 @@ ub = 1000;
 while (ub - lb > 1e-1)
     s1val = [];
     s2val = [];
-    ptry = (lb+ub)/2;
+    gtry = (lb+ub)/2;
 
     % evaluate parametrized SOS problem
-    sol1 = S1('p',[ptry;Vval]);
+    sol1 = S1('p',[gtry;Vval]);
 
     switch (S1.stats.UNIFIED_RETURN_STATUS)
-        case 'SOLVER_RET_SUCCESS', ...
+        case 'SOLVER_RET_SUCCESS'
             s1val = sol1.x;
-            lb = ptry;
-        case {'SOLVER_RET_INFEASIBLE' 'SOLVER_RET_NAN'}, ...
-                ub = ptry;
+            lb = gtry;
+        case {'SOLVER_RET_INFEASIBLE' 'SOLVER_RET_NAN'}
+            ub = gtry;
         otherwise, error('Failed.')
     end
 end
@@ -110,24 +101,23 @@ end
 gval = lb;
 
 
-
 % beta step: find largest possible stable level set
 lb = 0; 
 ub = 1000;
 
 % bisection
 while (ub - lb > 1e-1)
-    ptry = (lb+ub)/2;
+    btry = (lb+ub)/2;
 
     % evaluate parametrized SOS problem
-    sol2 = S2('p',[gval;ptry;Vval]);
+    sol2 = S2('p',[gval;btry;Vval]);
 
     switch (S2.stats.UNIFIED_RETURN_STATUS)
-        case 'SOLVER_RET_SUCCESS', ...
-            lb = ptry;
+        case 'SOLVER_RET_SUCCESS'
+            lb = btry;
             s2val = sol2.x;
-        case {'SOLVER_RET_INFEASIBLE' 'SOLVER_RET_NAN'}, ...
-            ub = ptry;
+        case {'SOLVER_RET_INFEASIBLE' 'SOLVER_RET_NAN'}
+            ub = btry;
         otherwise, error('Failed.')
     end
 end
@@ -141,22 +131,17 @@ end
 bval = lb;
 
 
+% V-step
+sol3 = S3('p',[bval,gval,s1val,s2val],'lbx',Vlb,'ubx',Vub);
 
- % V-step
- sol3 = S3('p',[bval,gval,s1val,s2val],'lbx',Vlb,'ubx',Vub);
+switch (S3.stats.UNIFIED_RETURN_STATUS)
+    case 'SOLVER_RET_SUCCESS'
+            Vval = sol3.x;
 
-
-    switch (S3.stats.UNIFIED_RETURN_STATUS)
-        case 'SOLVER_RET_SUCCESS',... 
-                Vval = sol3.x;
-
-        case {'SOLVER_RET_INFEASIBLE' 'SOLVER_RET_NAN'}
-            disp('V step infeasible')
-            break
-        otherwise, error('Failed.')
-    end
+    case {'SOLVER_RET_INFEASIBLE' 'SOLVER_RET_NAN'}
+        disp('V step infeasible')
+        break
+    otherwise, error('Failed.')
 end
-profile viewer
-toc
 
-
+end
