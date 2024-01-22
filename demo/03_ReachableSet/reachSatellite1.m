b5d04f03f97043bb3aae030fcd7fdf3e07e6632b
Jan Olucak <ac133867@uni-stuttgart.de>
2024-01-22 08:09:15 +0100

    update reachability problems

--- demo/03_ReachableSet/reachSatellite1.m
+++ demo/03_ReachableSet/reachSatellite1.m
@@ -8,12 +8,15 @@ x = casos.PS('x',6,1);
 u = casos.PS('u',3,1);
 t = casos.PS('t');
 
-x1 = x(1); 
-x2 = x(2); 
-x3 = x(3); 
-x4 = x(4);
-x5 = x(5);
-x6 = x(6);
+x_1 = x(1); 
+x_2 = x(2); 
+x_3 = x(3); 
+x_4 = x(4);
+x_5 = x(5);
+x_6 = x(6);
+
+
+
 
 J = diag([8970;9230;3830]);% Casini Parameter
 
@@ -43,7 +46,8 @@ R = diag([0.01 0.01 0.01]);
 
 Vval = x'*P*x;
 
-l = Vval-0.3;
+p = Vval*10;
+
 % rate constraints
 omega_max =  1;
 x_up  = [ omega_max omega_max omega_max]';
@@ -57,6 +61,10 @@ uup  = -ulow;
 
 umin = u-ulow;
 umax = uup - u;
+
+
+% ROA
+uc = ((-K*x)-ulow).*(uup - (-K*x));
 
 % time polynomial
 T = 1;
@@ -85,8 +93,193 @@ subplot(313)
 pcontour(subs(Vval,[x6;x1;x2;x3],zeros(4,1)),1,domain)
 
 
+%% Initial guess and terminal set computation
+
+f = subs(x_dot,u,-K*x);
+
 % Lyapunov function candidate
-V = casos.PS.sym('v',monomials([x;t],2));
+V = casos.PS.sym('v',monomials(x,2));
+
+% SOS multiplier
+s1 = casos.PS.sym('s1',monomials(x,0:2),'gram');
+s2 = casos.PS.sym('s2',monomials(x,0:2),'gram');
+
+s3 = casos.PS.sym('s3',monomials(x,0:2),[3 1],'gram');
+s4 = casos.PS.sym('s4',monomials(x,0:2),[3 1],'gram');
+
+% enforce positivity
+l = 1e-6*(x'*x);
+
+% level of stability
+g = casos.PS.sym('g');
+b = casos.PS.sym('b');
+
+% options
+opts               = struct('sossol','mosek');
+opts.error_on_fail = 0; 
+opts.max_iter      = 20;
+opts.verbose       = 1;
+opts.conf_interval = [-100 0]';
+
+opts.sossol_options.sdpsol_options.mosek_param.MSK_IPAR_BI_CLEAN_OPTIMIZER  = 'MSK_OPTIMIZER_INTPNT';
+opts.sossol_options.sdpsol_options.mosek_param.MSK_IPAR_INTPNT_BASIS        = 'MSK_BI_NEVER';
+
+%% setup solver
+disp('==================================================================')
+disp('Setting up solver ... ')
+tic
+% profile on  -historysize 50000000000000000
+
+V_sym = casos.PS.sym('V',basis(V));
+
+% solver 1: gamma-step
+sos1        = struct('x',[s1;s3;s4],'f',-g,'p',V_sym);
+sos1.('g')  = [
+               s1*(V_sym -g) - nabla(V_sym,x)*f - l; 
+               s3.*ones(3,1)*(V_sym - g) - gc;
+               s4.*ones(3,1)*(V_sym - g) + uc
+               ];
+
+% states + constraint are SOS cones
+opts.Kx = struct('s', 7);
+opts.Kc = struct('s', 7);
+
+S1 = casos.qcsossol('S1','bisection',sos1,opts);
+
+tend1 = toc;
+disp(['setting up solver 1 took ' num2str(tend1) ' s'])
+
+% solver 2: beta-step
+sos2        = struct('x',s2,'f',-b,'p',[V_sym;g]);
+sos2.('g')  = s2*(p-b)+g-V_sym;
+
+% states + constraint are SOS cones
+opts.Kx = struct('s', 1);
+opts.Kc = struct('s', 1);
+
+S2 = casos.qcsossol('S2','bisection',sos2,opts);
+
+tend2 = toc;
+disp(['setting up solver 2 took ' num2str(tend2-tend1) ' s'])
+
+s1_sym  = casos.PS.sym('s1',basis(s1));
+
+% solver 3: V-step
+s2_sym = casos.PS.sym('s2',basis(s2));
+s3_sym = casos.PS.sym('s3',basis(s3(1)),[3,1]);
+s4_sym = casos.PS.sym('s41',basis(s4(1)),[3,1]);
+
+% s1 = casos.PS.sym()
+Vlb = casos.PS(basis(V),-inf);
+Vub = casos.PS(basis(V),+inf);
+
+sos3       = struct('x',V,'p',[b;g;s1_sym;s2_sym;s3_sym;s4_sym]);
+sos3.('g') = [V-l;
+             s2_sym*(p - b) + g - V; 
+             s1_sym*(V - g)-nabla(V,x)*f - l;
+             s3_sym.*ones(3,1)*(V - g) - gc;
+             s4_sym.*ones(3,1)*(V - g) + uc;
+             ];
+
+opts    = struct;
+opts.Kx = struct('s', 0, 'l', 1); 
+opts.Kc = struct('s', 9);
+
+opts.sdpsol_options.mosek_param.MSK_IPAR_BI_CLEAN_OPTIMIZER = 'MSK_OPTIMIZER_INTPNT';
+opts.sdpsol_options.mosek_param.MSK_IPAR_INTPNT_BASIS       = 'MSK_BI_NEVER';
+
+
+S3 = casos.sossol('S','mosek',sos3,opts);
+
+tend3 = toc;
+
+%% gamma-beta-V-iteration
+disp(['setting up solver 3 took ' num2str(tend3-tend2) ' s'])
+disp('==================================================================')
+
+tic
+itermax = 1; % maximum number of iterations
+minIter = 1;   % minimum number of iterations (to check convergence)
+disp('Start G-B-V-iteration ...')
+for iter = 1:itermax
+
+    % gamma step
+    sol1 = S1('p',Vval);
+
+    switch (S1.stats.UNIFIED_RETURN_STATUS)
+        case 'SOLVER_RET_SUCCESS' 
+
+            gval   = double(-sol1.f);
+            s1val  = sol1.x(1);
+            s3val  = sol1.x(2:4);
+            s4val  = sol1.x(5:7);
+
+
+            disp(['G-step feasible in ' num2str(iter) '/' num2str(itermax)])
+
+        case {'SOLVER_RET_INFEASIBLE' 'SOLVER_RET_NAN'}
+
+            disp(['G-step infeasible in ' num2str(iter) '/' num2str(itermax)])
+            if iter == 1
+                break
+            end
+        otherwise
+            disp(['G-step infeasible in ' num2str(iter) '/' num2str(itermax)])
+
+    end
+
+   % beta step
+   sol2 = S2('p',[Vval;gval]);
+   switch (S2.stats.UNIFIED_RETURN_STATUS)
+        case 'SOLVER_RET_SUCCESS' 
+            bval = -sol2.f;
+            s2val = sol2.x;
+
+            disp(['B-step feasible in ' num2str(iter) '/' num2str(itermax) ])
+            
+        case {'SOLVER_RET_INFEASIBLE' 'SOLVER_RET_NAN'}
+
+            disp(['B-step infeasible in ' num2str(iter) '/' num2str(itermax)])
+
+                    
+        otherwise, disp(['B-step infeasible in ' num2str(iter) '/' num2str(itermax)])
+   end
+
+    % V-step
+    sol3 = S3('p',[bval; gval; s1val; s2val; s3val; s4val],...
+              'lbx',Vlb,'ubx',Vub);
+
+   switch (S3.stats.UNIFIED_RETURN_STATUS)
+        case 'SOLVER_RET_SUCCESS' 
+
+            Vval = sol3.x;
+            disp(['V-step feasible in ' num2str(iter) '/' num2str(itermax)])
+        case {'SOLVER_RET_INFEASIBLE' 'SOLVER_RET_NAN'}
+
+            disp(['V-step infeasible in ' num2str(iter) '/' num2str(itermax)])
+            break
+                    
+        otherwise, error('Failed.')
+   end
+
+    if iter <= minIter
+        gvalold = gval;
+    else
+        if abs(gval-gvalold) <= 1e-3
+            disp(['gamma is not changing anymore. Abored in iteration ' num2str(iter) '/' num2str(itermax)])
+            break
+        end
+    end
+
+end
+
+tend4  = toc;
+disp(['Finished G-B-V-iteration after ' num2str(tend4) ])
+l = Vval-gval;
+l = cleanpoly(l,1e-10);
+%% Reachability
+% Lyapunov function candidate
+V = casos.PS.sym('v',monomials([x;t],0:2));
 k = casos.PS.sym('k',monomials([x;t],0:2),[3 1]);
 
 % SOS multiplier
@@ -106,14 +299,14 @@ s7 = casos.PS.sym('s7',monomials(x,0:2),'gram');
 s8 = casos.PS.sym('s8',monomials(x,0:2),'gram');
 
 
-% level of stability
+% gamma level set
 g = casos.PS.sym('g');
 
 
 % options
 opts               = struct('sossol','mosek');
-% opts.error_on_fail = 0; 
 opts.max_iter      = 20;
+opts.verbose       = 1;
 opts.conf_interval = [-10 0]';
 
 opts.sossol_options.sdpsol_options.mosek_param.MSK_IPAR_BI_CLEAN_OPTIMIZER  = 'MSK_OPTIMIZER_INTPNT';
@@ -131,21 +324,25 @@ klb = casos.PS(basis(k),-inf);
 kub = casos.PS(basis(k),+inf);
 
 % solver 1: gamma-step
-sos1        = struct('x',[k;s1;s2;s3;s4;s5min;s6min;s5max;s6max;s7],...
+sos1        = struct('x',[k;s1;s2;...
+                         % s3;s4;...
+                          % s5min;s6min;s5max;s6max; ...
+                          % s7 ...
+                          ],...
                      'f',-g,...
                      'p',V_sym);
 
 sos1.('g')  = [
                s1*(V_sym -g) - nabla(V_sym,x)*subs(x_dot,u,k) - s2*hT; 
-               s3.*ones(3,1)*(V_sym - g) - gc - s4.*ones(3,1)*hT;
-               s5min.*ones(3,1)*(V_sym - g) + subs(umin,u,k) - s6min.*ones(3,1)*hT;
-               s5max.*ones(3,1)*(V_sym - g) + subs(umax,u,k) - s6max.*ones(3,1)*hT;
-               s7*subs(V_sym,t,T)-l;
+               % s3.*ones(3,1)*(V_sym - g) - gc - s4.*ones(3,1)*hT;
+               % s5min.*ones(3,1)*(V_sym - g) + subs(umin,u,k) - s6min.*ones(3,1)*hT;
+               % s5max.*ones(3,1)*(V_sym - g) + subs(umax,u,k) - s6max.*ones(3,1)*hT;
+               % s7*subs(V_sym,t,T)-l;
                ];
 
 % states + constraint are SOS cones
-opts.Kx = struct('s', 21,'l',3);
-opts.Kc = struct('s', 11);
+opts.Kx = struct('s', 2,'l',3);
+opts.Kc = struct('s', 1);
 opts.verbose = 1;
 S1 = casos.qcsossol('S1','bisection',sos1,opts);
 
@@ -167,30 +364,30 @@ s7_sym      = casos.PS.sym('s7',basis(s7));
 
 Vlb = casos.PS(basis(V),-inf);
 Vub = casos.PS(basis(V),+inf);
+% 
+% sos2       = struct('x',[V;s8],...
+%                     'p',[k;g; ...       
+%                          s1_sym;s2_sym;s3_sym;s4_sym; ...
+%                          s5min_sym;s5max_sym;s6min_sym;s6max_sym;...
+%                          s7_sym]);
+% sos2.('g') = [
+%                s1_sym*(V -g) - nabla(V,x)*subs(x_dot,u,k) - s2_sym*hT; 
+%                s3_sym.*ones(3,1)*(V - g) - gc - s4_sym.*ones(3,1)*hT;
+%                s5min_sym.*ones(3,1)*(V - g) + subs(umin,u,k) - s6min_sym.*ones(3,1)*hT;
+%                s5max_sym.*ones(3,1)*(V - g) + subs(umax,u,k) - s6max_sym.*ones(3,1)*hT;
+%                s7_sym*subs(V,t,T) - l;
+%                s8*(subs(V_sym,t,0)-g) + g - subs(V,t,0); 
+%              ];
 
-sos2       = struct('x',[V;s8],...
-                    'p',[k;g; ...       
-                         s1_sym;s2_sym;s3_sym;s4_sym; ...
-                         s5min_sym;s5max_sym;s6min_sym;s6max_sym;...
-                         s7_sym]);
-sos2.('g') = [
-               s1_sym*(V -g) - nabla(V,x)*subs(x_dot,u,k) - s2_sym*hT; 
-               s3_sym.*ones(3,1)*(V - g) - gc - s4_sym.*ones(3,1)*hT;
-               s5min_sym.*ones(3,1)*(V - g) + subs(umin,u,k) - s6min_sym.*ones(3,1)*hT;
-               s5max_sym.*ones(3,1)*(V - g) + subs(umax,u,k) - s6max_sym.*ones(3,1)*hT;
-               s7_sym*subs(V,t,T) - l;
-               s8*(subs(V_sym,t,0)-g) + g - subs(V,t,0); 
-             ];
-
-opts    = struct;
-opts.Kx = struct('s', 1, 'l', 1); 
-opts.Kc = struct('s', 12);
-
-opts.sdpsol_options.mosek_param.MSK_IPAR_BI_CLEAN_OPTIMIZER = 'MSK_OPTIMIZER_INTPNT';
-opts.sdpsol_options.mosek_param.MSK_IPAR_INTPNT_BASIS       = 'MSK_BI_NEVER';
-
-
-s2 = casos.sossol('S','mosek',sos2,opts);
+% opts    = struct;
+% opts.Kx = struct('s', 1, 'l', 1); 
+% opts.Kc = struct('s', 12);
+% 
+% opts.sdpsol_options.mosek_param.MSK_IPAR_BI_CLEAN_OPTIMIZER = 'MSK_OPTIMIZER_INTPNT';
+% opts.sdpsol_options.mosek_param.MSK_IPAR_INTPNT_BASIS       = 'MSK_BI_NEVER';
+% 
+% 
+% s2 = casos.sossol('S','mosek',sos2,opts);
 
 tend2 = toc;
 
@@ -199,8 +396,8 @@ disp(['setting up solver 2 took ' num2str(tend2-tend1) ' s'])
 disp('==================================================================')
 
 tic
-itermax = 100; % maximum number of iterations
-minIter = 5;   % minimum number of iterations (to check convergence)
+itermax = 1; % maximum number of iterations
+minIter = 1;   % minimum number of iterations (to check convergence)
 disp('Start G-B-V-iteration ...')
 for iter = 1:itermax
 
--- demo/03_ReachableSet/satReac.m
+++ demo/03_ReachableSet/satReac.m
@@ -4,38 +4,52 @@ clear
 clc
 
 % indeterminates
-x = casos.PS('x',2,1);
+x = casos.PS('x',6,1);
+u = casos.PS('u',3,1);
 t = casos.PS('t');
 
+x1 = x(1); 
+x2 = x(2); 
+x3 = x(3); 
+x4 = x(4);
+x5 = x(5);
+x6 = x(6);
 
 nx  =  length(x);
-nu  = 1;
-
-% system dynamics
-f = [x(2);
-        (1-x(1)^2)*x(2)-x(1)];
-
-gx = [0;1];
+nu  = length(u);
 
 
-% terminal region
-P = [6.4314    0.4580
-    0.4580    5.8227];
+J = diag([8970;9230;3830]);% Casini Parameter
 
-l = x'*P*x-1;              % l(x) \leq 0
+% skew-symmetric matrix
+skew = @(x) [   0  -x(3)  x(2); 
+              x(3)   0   -x(1); 
+             -x(2)  x(1)   0 ];
 
-% state constraint
-gc = [3*x(2)^2 + x(1)^2-1,...
-      -(x(1)-(-2))*(2-x(1)),...
-      -(x(2)-(-2))*(2-x(2))]';  % g(x) \leq 0
+
+B = @(sigma) (1-sigma'*sigma)*eye(3)+skew(sigma)+ 2*sigma*sigma';
+
+% dynamics
+x_dot =  [-inv(J)*skew(x(1:3))*J*x(1:3) + inv(J)*u; % omega_dot
+           1/4*B(x(4:6))*x(1:3)];  
+
+% rate constraints
+omega_max =  1;
+x_up  = [ omega_max omega_max omega_max]';
+x_low = [-omega_max -omega_max -omega_max]';
+
+gc   = -(x(1:3)-x_low).*(x_up-x(1:3));
+
+% control constraints
+umin = [-1 -1 -1]';
+umax  = -umin;
 
 
 ng = length(gc);
 
-
-% control constraint
-umin = -1;
-umax = 1;
+l = -0.269318 + 15838.7*x(1)^2 + 16403.9*x(2)^2 + 5433.14*x(3)^2 + 1480.93*x(1)...
+  *x(4) + 73.3*x(4)^2 + 1512.43*x(2)*x(5) + 74.0522*x(5)^2 + 765.567*x(3)*x(6) ...
+  + 53.4006*x(6)^2;
 
 % Time horizon
 T     = 1;
@@ -45,8 +59,8 @@ hT = (t)*(T-t);
 
 
 % Storage function and control law
-V = casos.PS.sym('v',monomials([x;t],0:4));
-k = casos.PS.sym('k',monomials([x;t],0:4));
+V = casos.PS.sym('v',monomials([x;t],0:2));
+k = casos.PS.sym('k',monomials([x;t],0:2),[nu 1]);
 
 % SOS multiplier
 
@@ -79,7 +93,7 @@ sos1 = struct('x',[k;s1;s2;s3;s4;s51;s52;s61;s62;s7]... % decsision variables
              ,'f',-g,...   % cost
               'p',V');     % parameter
 
-sos1.('g') = [s1*(V-g) - s2*hT - nabla(V,t) - nabla(V,x)*(f + gx*k);
+sos1.('g') = [s1*(V-g) - s2*hT - nabla(V,t) - nabla(V,x)*subs(x_dot,u,k);
               k - umin  + s51.*ones(nu,1)*(V-g) - s61.*ones(nu,1)*hT;
               umax - k   + s52.*ones(nu,1)*(V-g) - s62.*ones(nu,1)*hT;
               s7*(subs(V,t,T)-g)    -  l;
@@ -87,20 +101,24 @@ sos1.('g') = [s1*(V-g) - s2*hT - nabla(V,t) - nabla(V,x)*(f + gx*k);
 
 % states + constraint are SOS cones
 opts = struct('sossol','mosek');
-opts.Kx = struct('s', 2+2*ng+4*nu+1,'l',1);
+opts.Kx = struct('s', 2+2*ng+4*nu+1,'l',nu);
 opts.Kc = struct('s', 2+ng+2*nu);
 opts.error_on_fail = 0; 
 opts.verbose = 1;
 opts.conf_interval = [-10 0]';
 
-klb = casos.PS(basis(k),-inf);
-kub = casos.PS(basis(k),+inf);
+klb = casos.PS(basis(k(1)),-inf)*ones(nu,1);
+kub = casos.PS(basis(k(1)),+inf)*ones(nu,1);
 
 opts.sossol_options.sdpsol_options.mosek_param.MSK_IPAR_BI_CLEAN_OPTIMIZER = 'MSK_OPTIMIZER_INTPNT';
 opts.sossol_options.sdpsol_options.mosek_param.MSK_IPAR_INTPNT_BASIS = 'MSK_BI_NEVER';
 
 % solver for S-step
 S1 = casos.qcsossol('S1','bisection',sos1,opts);
+
+tend1 = toc;
+disp(['took ' num2str(tend1) ' s'])
+
 
 % solver 2: V-step
 s1sym  = casos.PS.sym('s1',basis(s1));
@@ -114,13 +132,13 @@ s62sym = casos.PS.sym('s62',basis(s62(1)),[nu 1]);
 s7sym  = casos.PS.sym('s7',basis(s7));
 
 Vsym = casos.PS.sym('V',basis(V));
-ksym = casos.PS.sym('k',basis(k));
+ksym = casos.PS.sym('k',basis(k(1)),[nu 1]);
 
 
 sos2 = struct('x',[V,s8]',...                                                                        % dec. variable
               'p',[ksym;s1sym;s2sym;s3sym;s4sym;s51sym;s52sym;s61sym;s62sym;s7sym;Vsym;g]);   % parameter
 
-sos2.('g') = [s1sym*(V-g)  - s2sym*hT     - nabla(V,t) - nabla(V,x)*(f + gx*ksym);
+sos2.('g') = [s1sym*(V-g)  - s2sym*hT     - nabla(V,t) - nabla(V,x)*subs(x_dot,u,ksym);
               ksym - umin  + s51sym.*ones(nu,1)*(V-g) - s61sym.*ones(nu,1)*hT;
               umax - ksym  + s52sym.*ones(nu,1)*(V-g) - s62sym.*ones(nu,1)*hT;
               s3sym.*ones(ng,1)*(V-g)  - s4sym.*ones(ng,1)*hT - gc;
@@ -143,14 +161,14 @@ opts.sdpsol_options.mosek_param.MSK_IPAR_INTPNT_BASIS = 'MSK_BI_NEVER';
 S2 = casos.sossol('S2','mosek',sos2,opts);
 
 
-tend1 = toc;
-disp(['took ' num2str(tend1) ' s'])
+tend2 = toc;
+disp(['took ' num2str(tend2-tend1) ' s'])
 
 
 %% gamma-beta-V-iteration
 
 % initial guess storage function
-Vval = x'*P*x;
+Vval = l;
 
 tic
 
