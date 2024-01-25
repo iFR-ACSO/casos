2d7c54aa1ac195c346b702262f69c7117be7cfaf
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2024-01-25 11:27:51 +0100

    Improve polynomial matrix product by recursion.

--- +casos/@PS/prod.m
+++ +casos/@PS/prod.m
@@ -21,7 +21,8 @@ if isempty(A)
     B = casos.PS.ones(sz);
     return
 
-elseif size(A,dim) == 1
+elseif isequal(dim,'all') && isscalar(A) ...
+        || ~isequal(dim,'all') && size(A,dim) == 1
     % nothing to do
     B = A;
     return
@@ -30,73 +31,14 @@ end
 % else:
 B = casos.PS;
 
-nt = A.nterm;
-nv = A.nvars;
-
 % reshape input coefficient matrix
 [cfa,sz] = prepareMatrixOp(A.coeffs,A.matdim,dim);
 
 % multiply entries
-L = numel(A)/prod(sz);
-nb = prod(sz);
-% (sum_a c_a[1]*x^a).* ... .* (sum_a c_a[L]*x^a)
-%  = sum_a1 ... sum_aL (c_a1[1].* ... .* c_aL[L])*x^(a1+...+aL)
-I = cell(1,L);
-[I{:}] = ndgrid(1:nt); I = cellfun(@(c) c(:),I,'UniformOutput',false);
-idx = horzcat(I{:});
-
-% permute degree matrix
-%
-%       | dax ... day |      | da1x ... da1y        |
-%   D = |  :       :  |  ->  |   :        :     ... |
-%       | dzx ... dzy |      | daLx ... daLy        |
-%
-dga = reshape(A.degmat(idx',:),L,nv*nt^L);
-% combine degrees
-dgb = sum(dga,1);
-
-% permute and combine coefficients matrix
-% for details on cfa, see PREPAREMATRIXOP
-switch (dim)
-    case 'all'
-        % compute total product
-        % rows are elements of coefficients c_a
-        row = idx;              % select coefficient
-        col = repmat(1:L,nt^L,1);   % select element
-        ind = sub2ind(size(cfa),row,col);
-        C = reshape(cfa(ind),nt^L,L);
-
-        % combine coefficients
-        cfb = sx_prod(C,2);
-
-    case 2
-        % compute product along second dimension
-        % rows are rows of c_a, sorted by term
-        row = kron((0:nb-1)',nt*ones(nt^L,L)) + repmat(idx,nb,1);
-        col = repmat(1:L,nb*nt^L,1);    % select row
-        ind = sub2ind(size(cfa),row,col);
-        C = reshape(cfa(ind),nb*nt^L,L);
-
-        % combine coefficients
-        cfb = sx_prod(C,2);
-
-    case 1
-        % compute product along first dimension
-        % columns are colums of c_a, sorted by coefficient
-        term0 = nb*(0:nt-1)';
-        row = repmat(1:L,nb*nt^L,1);    % select column
-        col = kron(term0(idx),ones(nb,1)) + repmat((1:nb)',nt^L,L);
-        ind = sub2ind(size(cfa),row,col);
-        C = reshape(cfa(ind'),L,nb*nt^L);
-
-        % combine coefficients
-        cfb = sx_prod(C,1);
-end
+[cfb,degmat] = prod_internal(cfa,A.degmat,dim,numel(A)/prod(sz),prod(sz));
 
 % reshape output coefficient matrix
 coeffs = finishMatrixOp(cfb,sz,dim);
-% reshape output degree matrix
-degmat = reshape(dgb,nt^L,nv);
 
 % make degree matrix unique
 [coeffs,degmat] = uniqueDeg(coeffs, degmat);
@@ -109,5 +51,111 @@ B.coeffs = coeffs;
 B.degmat = degmat;
 B.indets = indets;
 B.matdim = sz;
+
+end
+
+function [coeffs,degmat] = prod_internal(coeffs,degmat,dim,L,nb)
+% Compute product of matrix elements.
+
+nt = size(degmat,1);
+
+if L <= 1
+    % nothing to do
+
+elseif L == 2 || (L*nt^L) > 1e6
+    % two elements to multiply or
+    % number of permutations exceeds memory
+    L1 = ceil(L/2);
+    L2 = L - L1;
+    % divide and conquer
+    switch (dim)
+        case {2 'all'}
+            % compute product along second dimension
+            cfs = mat2cell(coeffs,nb*nt,[L1 L2]);
+
+        case 1
+            % compute product along first dimension
+            cfs = mat2cell(coeffs,[L1 L2],nb*nt);
+    end
+    % compute partial products
+    [cfa,dga] = prod_internal(cfs{1},degmat,dim,L1,nb);
+    [cfb,dgb] = prod_internal(cfs{2},degmat,dim,L2,nb);
+
+    nta = size(dga,1);
+    ntb = size(dgb,1);
+
+    % multiply partial products
+    % see TIMES for details
+    switch (dim)
+        case {2 'all'}
+            % multiply rows
+            coeffs = kron(cfa,ones(ntb,1)) .* kron(ones(nta,1),cfb);
+
+        case 1
+            % multiply columns
+            coeffs = kron(cfa,ones(1,ntb)) .* kron(ones(1,nta),cfb);
+    end
+    degmat = kron(dga,ones(ntb,1)) +  kron(ones(nta,1),dgb);
+
+else
+    % (sum_a c_a[1]*x^a).* ... .* (sum_a c_a[L]*x^a)
+    %  = sum_a1 ... sum_aL (c_a1[1].* ... .* c_aL[L])*x^(a1+...+aL)
+    I = cell(1,L);
+    [I{:}] = ndgrid(1:nt); I = cellfun(@(c) c(:),I,'UniformOutput',false);
+    idx = horzcat(I{:});
+
+    nv = size(degmat,2);
+    
+    % permute degree matrix
+    %
+    %       | dax ... day |      | da1x ... da1y        |
+    %   D = |  :       :  |  ->  |   :        :     ... |
+    %       | dzx ... dzy |      | daLx ... daLy        |
+    %
+    dga = reshape(degmat(idx',:),L,nv*nt^L);
+    % combine degrees
+    dgb = sum(dga,1);
+    
+    % permute and combine coefficients matrix
+    % for details on cfa, see PREPAREMATRIXOP
+    switch (dim)
+        case 'all'
+            % compute total product
+            % rows are elements of coefficients c_a
+            row = idx;              % select coefficient
+            col = repmat(1:L,nt^L,1);   % select element
+            ind = sub2ind(size(coeffs),row,col);
+            C = reshape(coeffs(ind),nt^L,L);
+    
+            % combine coefficients
+            coeffs = sx_prod(C,2);
+    
+        case 2
+            % compute product along second dimension
+            % rows are rows of c_a, sorted by term
+            row = kron((0:nb-1)',nt*ones(nt^L,L)) + repmat(idx,nb,1);
+            col = repmat(1:L,nb*nt^L,1);    % select row
+            ind = sub2ind(size(coeffs),row,col);
+            C = reshape(coeffs(ind),nb*nt^L,L);
+    
+            % combine coefficients
+            coeffs = sx_prod(C,2);
+    
+        case 1
+            % compute product along first dimension
+            % columns are colums of c_a, sorted by coefficient
+            term0 = nb*(0:nt-1)';
+            row = repmat(1:L,nb*nt^L,1);    % select column
+            col = kron(term0(idx),ones(nb,1)) + repmat((1:nb)',nt^L,L);
+            ind = sub2ind(size(coeffs),row,col);
+            C = reshape(coeffs(ind'),L,nb*nt^L);
+    
+            % combine coefficients
+            coeffs = sx_prod(C,1);
+    end
+    
+    % reshape output degree matrix
+    degmat = reshape(dgb,nt^L,nv);
+end
 
 end
