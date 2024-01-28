4b496f544d5300c9589733cdb6caaeff8e44efc8
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2024-01-28 17:55:23 +0100

    Matrix product: Handle constant matrix and matrix of monomials.

--- +casos/@PS/prod.m
+++ +casos/@PS/prod.m
@@ -31,17 +31,96 @@ end
 % else:
 B = casos.PS;
 
+nt = A.nterm;
+nv = A.nvars;
+ne = A.numel;
+sz = A.matdim;
+
+% check number of coefficients per matrix component
+idx = find(sparsity(A.coeffs));
+[ia,ja] = ind2sub(size(A.coeffs),idx);
+
+if nt == 1 
+    % polynomial is a constant matrix 
+    % or prod(a) = prod(c)*(x^d)^L
+    
+    % reshape input coefficient matrix
+    [cfa,sz] = prepareMatrixOp(A.coeffs,sz,dim);
+
+    % multiply entries of coefficient
+    cfb = evalMatrixOp(cfa,@sx_prod,dim);
+
+    % reshape output coefficient matrix
+    coeffs = finishMatrixOp(cfb,sz,dim);
+
+    % number of entries to multiply
+    L = ne/prod(sz);
+    % multiply degrees (if any)
+    degmat = L*A.degmat;
+
+elseif issorted(ja,'strictascend')
+    % polynomial is a matrix of monomials
+    % match degrees to nonzero coefficients
+    dga = sparse(ne,nv);
+    dga(ja,:) = A.degmat(ia,:);
+
+    % combine degrees
+    switch (dim)
+        case 'all'
+            % matrix operation over all elements
+            % sum all degrees
+            degmat = sum(dga,1);
+
+        case 1
+            % matrix operation along first dimension
+            dgb = reshape(dga,sz(1),sz(2)*nv);
+            % sum degrees columnwise
+            dgc = sum(dgb,1);
+            % reshape to degree matrix
+            degmat = reshape(dgc,sz(2),nv);
+
+        case 2
+            % matrix operation along second dimension
+            dgb = reshape(dga',sz(1)*nv,sz(2));
+            % sum degrees rowwise
+            dgc = sum(dgb,2);
+            % reshape to degree matrix
+            degmat = reshape(dgc,nv,sz(1))';
+
+        otherwise
+            error('Invalid dimension input')
+    end
+
+    % combine coefficients
+    coeff = sum(A.coeffs,1);
+
+    % reshape input coefficient matrix
+    [cfa,sz] = prepareMatrixOp(coeff,sz,dim);
+
+    % multiply coefficients
+    cfb = evalMatrixOp(cfa,@sx_prod,dim);
+
+    % expand to coefficient matrix
+    coeffs = diag(cfb);
+
+    % make degree matrix unique
+    [coeffs,degmat] = uniqueDeg(coeffs, degmat);
+
+else
+
 % reshape input coefficient matrix
-[cfa,sz] = prepareMatrixOp(A.coeffs,A.matdim,dim);
+[cfa,sz] = prepareMatrixOp(A.coeffs,sz,dim);
+
+% number of entries to multiply
+L = ne/prod(sz);
 
 % multiply entries
-[cfb,degmat] = prod_internal(cfa,A.degmat,dim,numel(A)/prod(sz),prod(sz));
+[cfb,degmat] = prod_internal(cfa,A.degmat,dim,L,prod(sz));
 
 % reshape output coefficient matrix
 coeffs = finishMatrixOp(cfb,sz,dim);
 
-% make degree matrix unique
-% [coeffs,degmat] = uniqueDeg(coeffs, degmat);
+end
 
 % remove zero terms
 [coeffs,degmat,indets] = removeZero(coeffs,degmat,A.indets);
@@ -96,6 +175,7 @@ elseif L == 2 || (L*nt^L) > 1e6
             % multiply columns
             coeffs = kron(cfa,ones(1,ntb)) .* kron(ones(1,nta),cfb);
     end
+    % combine degrees (see TIMES)
     degmat = kron(dga,ones(ntb,1)) +  kron(ones(nta,1),dgb);
 
 else
@@ -153,6 +233,9 @@ else
     
             % combine coefficients
             coeffs = sx_prod(C,1);
+
+        otherwise
+            error('Invalid dimension input.')
     end
     
     % reshape output degree matrix
