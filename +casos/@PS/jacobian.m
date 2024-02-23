7ed9950e3d3f7151c2fe689aaf99c3deaebeb966
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2024-02-23 19:32:27 +0100

    Symbolic jacobian: return jacobian of coefficients w/ basis.

--- +casos/@PS/jacobian.m
+++ +casos/@PS/jacobian.m
@@ -1,4 +1,4 @@
-function g = jacobian(f,x)
+function [G,zi,zo] = jacobian(f,x)
 % Compute symbolic Jacobian matrix of vector polynomial expression.
 %
 % Use of JACOBIAN for the polynomial Jacobian matrix is deprecated. 
@@ -7,11 +7,21 @@ function g = jacobian(f,x)
 if is_indet(x)
     % DEPRECATED: compute polynomial Jacobian matrix.
     warning('DEPRECATED: Use NABLA for the polynomial Jacobian.')
-    g = nabla(f,x);
+    G = nabla(f,x);
     return
 end
 
 % else
+f = casos.PS(f);
+x = casos.PS(x);
+
 assert(is_symbolic(x),'Second argument must be symbolic polynomial.')
+
+% project to basis
+[X,zi] = poly2basis(x);
+[F,zo] = poly2basis(f);
+
+% G is jacobian of coefficients
+G = jacobian(F,X);
 
 end
