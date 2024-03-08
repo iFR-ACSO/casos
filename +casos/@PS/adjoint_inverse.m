4b41e53dbddda52e8b92283541d2e16827ab3ae3
Torbjørn Cunis <torbjoern.cunis@gmail.com>
2024-03-08 05:29:01 -0500

    Bug-fix: Dual variables for convex sum-of-squares problems (#48)
    * Bug-fix:  empty Gram basis.

--- +casos/@PS/adjoint_inverse.m
+++ +casos/@PS/adjoint_inverse.m
@@ -1,6 +1,13 @@
 function Z = adjoint_inverse(obj)
 % Compute the adjoint-inverse of a Gram basis.
 
+if obj.nterm == 0
+    % nothing to do
+    Z = obj;
+    return
+end
+
+% else:
 scale = sum(obj.coeffs,2);
 
 % new polynomial
