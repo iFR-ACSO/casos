e9fe2bedaa0968c77c96eb6fb0d8dde1be40d349
Fabian Geyer <fbn.geyer@gmail.com>
2024-04-03 16:27:27 +0200

    cleanup

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -15,16 +15,15 @@ author = 'Torbjørn Cunis, Jan Olucak, Renato Loureiro, Fabian Geyer'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
 sys.path.insert(0, os.path.abspath(os.path.join("..", "..")))
 matlab_src_dir = os.path.abspath("../..")
-
 
 extensions = [
     'sphinx.ext.autodoc',
     'sphinxcontrib.matlab',
     'sphinx.ext.napoleon',
 ]
-
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
