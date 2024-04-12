b065dd4f20bc25d47ecd4cff2b08e89751a5699a
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2024-04-12 13:39:52 +0200

    Remove sparse-zero terms from sparsity pattern.

--- +casos/@Sparsity/private/removeZero.m
+++ +casos/@Sparsity/private/removeZero.m
@@ -4,10 +4,8 @@ function [coeffs,degmat,indets] = removeZero(coeffs,degmat,indets)
 
 nel = size(coeffs,2);
 
-if ~isa(coeffs,'casadi.Sparsity')
 % remove degrees with zero coefficient
 [coeffs,degmat] = removeCoeffs(coeffs,degmat);
-end
 
 if nargin > 2
 % remove unused variables
@@ -32,22 +30,48 @@ end
 function [coeffs,degmat] = removeCoeffs(coeffs,degmat)
 % Remove terms with zero coefficient and update degree matrix.
     
-    % sparsity pattern of coefficients
-    S1 = sparsity(coeffs);
-    % sparsity pattern without zeros
-    S0 = sparsity(sparsify(coeffs));
+    if isa(coeffs,'casadi.Sparsity')
+        % nonzero row and column indices (triplet)
+        [ii,jj] = ind2sub(size(coeffs),find(coeffs));
 
-    % nonzero coefficients with linear indices
-    [i0,j0] = ind2sub(size(coeffs),find(S0));   % sparse zeros
-    [~, j1] = ind2sub(size(coeffs),find(S1));   % full zeros
+        % sparsity pattern without all-sparse rows
+        [coeffs,nr] = nonzeroPattern(size(coeffs),ii,jj);
+    else
+        % sparsity pattern of coefficients
+        S1 = sparsity(coeffs);
+        % sparsity pattern without zeros
+        S0 = sparsity(sparsify(coeffs));
     
-    % detect all-zero columns (full zero)
-    iz = ~ismember(j1,j0);
+        % nonzero coefficients with linear indices
+        [i0,j0] = ind2sub(size(coeffs),find(S0));   % sparse zeros
+        [~, j1] = ind2sub(size(coeffs),find(S1));   % full zeros
+        
+        % detect all-zero columns (full zero)
+        iz = ~ismember(j1,j0);
+    
+        % assign full zeros to first not all-sparse row
+        ii = [i0 repmat(max([min(i0) 1]),1,nnz(iz))];
+        jj = [j0 j1(iz)];
+    
+        % sparsity pattern without all-sparse rows
+        [S,nr] = nonzeroPattern(size(coeffs),ii,jj);
 
-    % assign full zeros to first not all-sparse row
-    ii = [i0 repmat(max([min(i0) 1]),1,nnz(iz))];
-    jj = [j0 j1(iz)];
-    
+        % assign nonzero coefficients to sparsity pattern
+        coeffs = project(coeffs(nr,:),S);
+    end
+
+    if isempty(ii)
+        % only zero coefficients
+        degmat = sparse(1,size(degmat,2));
+    else
+        % remove corresponding degree matrix entries
+        degmat = degmat(nr,:);
+    end
+end
+
+function [S,nr] = nonzeroPattern(sz,ii,jj)
+% Return sparsity pattern without all-sparse rows
+
     % identify all-zero rows (zero coefficient)
     [nr,~,ir] = unique(ii);
 
@@ -55,17 +79,7 @@ function [coeffs,degmat] = removeCoeffs(coeffs,degmat)
     I = 1:length(nr);
     
     % sparsity pattern without all-sparse rows (note: 0-based index)
-    S = casadi.Sparsity.triplet(length(nr),size(coeffs,2),I(ir)-1,jj-1);
-    % assign nonzero coefficients to sparsity pattern
-    coeffs = project(coeffs(nr,:),S);
-
-    if isempty(i0)
-        % only zero coefficients
-        degmat = sparse(1,size(degmat,2));
-    else
-        % remove corresponding degree matrix entries
-        degmat = degmat(nr,:);
-    end
+    S = casadi.Sparsity.triplet(length(nr),sz(2),I(ir)-1,jj-1);
 end
 
 function [degmat,indets] = removeDegVar(degmat,indets)
