b9bf19163165b5a34bfac2d7bee5a66fdbc2532a
JOlCode <61011693+JOlCode@users.noreply.github.com>
2024-04-18 08:18:47 +0200

    update to main version of bildproblem; for some reason it was not update during the merge

--- +casos/+package/+solvers/@SossdpRelaxation/buildproblem.m
+++ +casos/+package/+solvers/@SossdpRelaxation/buildproblem.m
@@ -44,11 +44,8 @@ gdiff = (sos.g - [zeros(Ml,1); casos.PS(Zgram_g,Qgram_g)]);
 % get cost function
 [Qlin_f,Zlin_f] = poly2basis(sos.f,[],[],false);
 
-
-
-
 % build SDP problem
-sdp.x = [Qlin_x'; Qgram_x; Qgram_g];
+sdp.x = [Qlin_x; Qgram_x; Qgram_g];
 sdp.f = Qlin_f;
 sdp.g = Qdiff_g;
 sdp.p = Qlin_p;
