275983d5900620813dc92e96480ed1539b12a64d
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2024-05-14 20:12:26 +0200

    Project irregular coefficients (only PD).

--- +casos/@Sparsity/coeff_project.m
+++ +casos/@Sparsity/coeff_project.m
@@ -6,6 +6,13 @@ assert(~isa(coeffs,'casadi.Sparsity'),'Notify the developers.')
 % expand coefficients to match degrees
 [S,S_coeffs,coeffs] = coeff_expand(S,obj,S.coeffs,coeffs);
 
+if isa(coeffs,'casadi.DM') && ~is_regular(coeffs)
+    % handle irregular coefficients (inf, nan)
+    coeffs = extend_irregular(coeffs,+inf);
+    coeffs = extend_irregular(coeffs,-inf);
+    coeffs = extend_irregular(coeffs,nan);
+end
+
 % project onto coefficient sparsity pattern
 coeffs = project(coeffs,S_coeffs);
 
@@ -15,4 +22,18 @@ coeffs = project(coeffs,S_coeffs);
 % store coefficients
 S = set_coefficients(S,coeffs);
 
+end
+
+function coeffs = extend_irregular(coeffs,num)
+% If the coefficient matrix contains an irregular number, 
+% extend that number over all terms.
+
+    S_coeffs = sparsity(sparsify(coeffs == num));
+    % detect columns (= entry of coefficients) that are irregular
+    [~,j1] = get_triplet(S_coeffs);
+
+    if isempty(j1), return; end % nothing to do
+
+    % make entry irregular for all terms
+    coeffs(:,unique(j1)+1) = num;
 end
