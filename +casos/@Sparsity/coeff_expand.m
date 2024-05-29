dede389160f3f6c38d2cd863f4473a8b486293c4
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2024-05-29 23:07:09 +0200

    Bug-fix expansion with two outputs.

--- +casos/@Sparsity/coeff_expand.m
+++ +casos/@Sparsity/coeff_expand.m
@@ -2,6 +2,6 @@ function [cf1,cf2] = coeff_expand(S1,S2,cfa,cfb)
 % Expand polynomial coefficient matrices to degrees.
 
 % expand coefficients
-[cf1,cf2] = expand_internal(S1,S2,cfa,cfb,(nargin > 2));
+[cf1,cf2] = expand_internal(S1,S2,cfa,cfb,(nargout < 2));
 
 end
