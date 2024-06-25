f8c1f83d93bb387cd7e751a74c1f3ef00757b0ce
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2024-06-25 18:13:07 +0200

    Transpose vectors efficiently.

--- +casos/@Sparsity/coeff_transpose.m
+++ +casos/@Sparsity/coeff_transpose.m
@@ -7,21 +7,26 @@ S = casos.Sparsity;
 sza = size(obj);
 szb = [sza(2) sza(1)];
 
-% indices
-[ib,jb] = ind2sub(szb,1:prod(szb));
+if ~isvector(obj)
+    % transpose matrix
 
-% transpose
-idx = sub2ind(sza,jb,ib);
-
-if isa(coeffs,'casadi.Sparsity')
-    % subindex into sparsity pattern
-    % NOTE: Casadi has zero-based index
-    coeffs = sub(coeffs,0:(obj.nterm-1),idx-1);
-
-else
-    % subscript into coefficient matrix
-    coeffs = coeffs(:,idx);
+    % indices
+    [ib,jb] = ind2sub(szb,1:prod(szb));
+    
+    % transpose
+    idx = sub2ind(sza,jb,ib);
+    
+    if isa(coeffs,'casadi.Sparsity')
+        % subindex into sparsity pattern
+        % NOTE: Casadi has zero-based index
+        coeffs = sub(coeffs,0:(obj.nterm-1),idx-1);
+    
+    else
+        % subscript into coefficient matrix
+        coeffs = coeffs(:,idx);
+    end
 end
+% nothing to do for a vector
 
 % new sparsity pattern
 S.degmat = obj.degmat;
