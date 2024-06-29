9180bdc3563660fb5d9270e115f0c883134cc68f
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2024-06-29 16:23:50 +0200

    Bug-fix: Polynomial norm of row or matrix.

--- +casos/+package/+core/@Polynomial/pnorm2.m
+++ +casos/+package/+core/@Polynomial/pnorm2.m
@@ -3,11 +3,12 @@ function r = pnorm2(a)
 %
 % Note: This 2-norm does NOT correspond to the norm induced by dot.
 
-% transpose
-[St,cft] = coeff_transpose(a.get_sparsity,a.coeffs);
+% reshape to vector
+a = reshape(a,numel(a),1);
+Sa = a.get_sparsity;
 
 % TODO: use internal operations
-[Sb,cfb] = mtimes_internal(St,a.get_sparsity,cft,a.coeffs);
+[Sb,cfb] = mtimes_internal(T(Sa),Sa,a.coeffs,a.coeffs);
 
 % integral over [0 1]
 r = coeff_properint(Sb,cfb);
