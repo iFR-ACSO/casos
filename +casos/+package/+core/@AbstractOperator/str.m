3d2d7ca57552acac35de6a06f21169276d149af3
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2024-07-01 14:41:57 +0200

    Bug-fix: print input and output dimensions of operators.

--- +casos/+package/+core/@AbstractOperator/str.m
+++ +casos/+package/+core/@AbstractOperator/str.m
@@ -1,9 +1,9 @@
 function s = str(obj)
 % Return string representation.
 
-sp_in = signature(obj.sparsity_in);
-sp_out = signature(obj.sparsity_out);
+sp_in = to_char(obj.sparsity_in);
+sp_out = to_char(obj.sparsity_out);
 
-s = compose('Operator: [%s] -> [%s]',sp_in{:},sp_out{:});
+s = compose('Operator: [%s] -> [%s]',sp_in,sp_out);
 
 end
