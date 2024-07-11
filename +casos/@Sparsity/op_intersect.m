eb9b153d0a369d2efa2c61a794d055d9c5bcd1c2
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2024-07-11 16:16:37 +0200

    Bug-fix: compute relative indices before removing zeros.

--- +casos/@Sparsity/op_intersect.m
+++ +casos/@Sparsity/op_intersect.m
@@ -10,8 +10,17 @@ S = casos.Sparsity;
 % expand coefficient matrices
 [cfa,cfb,degmat,indets] = expand_internal(S1,S2,S1.coeffs,S2.coeffs);
 
-% remove zero terms from intersection of cfa and cfb
-[coeffs,degmat,indets] = removeZero(cfa*cfb,degmat,indets);
+% intersection of cfa and cfb
+coeffs = cfa*cfb;
+
+if nargout > 1
+    % return indices
+    I1 = ismembc2(find(coeffs),find(cfa));
+    I2 = ismembc2(find(coeffs),find(cfb));
+end
+
+% remove zero terms from intersection
+[coeffs,degmat,indets] = removeZero(coeffs,degmat,indets);
 
 % return sparsity pattern
 S.degmat = degmat;
@@ -19,11 +28,5 @@ S.indets = indets;
 S.matdim = size(S1);
 % store coefficients
 S = set_coefficients(S,coeffs);
-
-if nargout > 1
-    % return indices
-    I1 = ismembc2(find(coeffs),find(cfa));
-    I2 = ismembc2(find(coeffs),find(cfb));
-end
 
 end
