cb4cc19faff2f67bfd7d6c61b174480aa5913e91
Renato Loureiro <renato.loureiro@ifr.uni-stuttgart.de>
2024-07-19 10:55:10 +0200

    QuasiConvex: allow for dsos and sdsos cones.

--- +casos/+package/+solvers/@QuasiconvBisection/buildproblem.m
+++ +casos/+package/+solvers/@QuasiconvBisection/buildproblem.m
@@ -21,17 +21,24 @@ n = length(qcsos.x);
 m = length(qcsos.g);
 
 % get cone dimensions
+% get cone dimensions for the decision variables
 Nl = get_dimension(obj.get_cones,opts.Kx,'lin');
 Ns = get_dimension(obj.get_cones,opts.Kx,'sos');
+Nds  = get_dimension(obj.get_cones,opts.Kx,'dsos');
+Nsds = get_dimension(obj.get_cones,opts.Kx,'sdsos');
+
+% get cone dimensions for the constraints
 Ml = get_dimension(obj.get_cones,opts.Kc,'lin');
 Ms = get_dimension(obj.get_cones,opts.Kc,'sos');
+Mds  = get_dimension(obj.get_cones,opts.Kc,'dsos');
+Msds = get_dimension(obj.get_cones,opts.Kc,'sdsos');
 
-assert(n == (Nl + Ns), 'Dimension of Kx must be equal to number of variables (%d).', n);
-assert(m == (Ml + Ms), 'Dimension of Kc must be equal to number of constraints (%d).', m)
+assert(n == (Nl + Ns + Nds + Nsds), 'Dimension of Kx must be equal to number of variables (%d).', n);
+assert(m == (Ml + Ms + Mds + Msds), 'Dimension of Kc must be equal to number of constraints (%d).', m)
 
 % select sum-of-squares variables and constraints
-Is = [false(Nl,1); true(Ns,1)];
-Js = [false(Ml,1); true(Ms,1)];
+Is = [false(Nl,1); true(Ns,1); true(Nds,1); true(Nsds,1)];
+Js = [false(Ml,1); true(Ms,1); true(Mds,1); true(Msds,1)];
 
 % build SOS problem
 sos.x = qcsos.x;
@@ -41,8 +48,8 @@ sos.p = [qcsos.p; dvar];
 
 % SOS options
 sosopt = opts.sossol_options;
-sosopt.Kx = struct('lin',Nl,'sos',Ns);
-sosopt.Kc = struct('lin',Ml,'sos',Ms);
+sosopt.Kx = struct('lin',Nl,'sos',Ns,'dsos',Nds,'sdsos',Nsds);
+sosopt.Kc = struct('lin',Ml,'sos',Ms,'dsos',Mds,'sdsos',Msds);
 sosopt.error_on_fail = false;
 
 % initialize convex SOS solver
