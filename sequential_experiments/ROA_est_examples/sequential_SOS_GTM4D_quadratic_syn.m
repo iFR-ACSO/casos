eda887ece88d2b821a911684a38dfdb92a0be55d
JOlCode <61011693+JOlCode@users.noreply.github.com>
2024-08-01 08:24:23 +0200

    moved examples for region of attraction estimation to new folder including helper functions

---
+++ sequential_experiments/ROA_est_examples/sequential_SOS_GTM4D_quadratic_syn.m
@@ -0,0 +1,179 @@
+% Estimate region of the Generic Transport Model
+% See Chakraborty et al. 2011 (CEP) for details.
+
+
+import casos.toolboxes.sosopt.plinearize
+import casos.toolboxes.sosopt.pcontour
+import casos.toolboxes.sosopt.cleanpoly
+import casos.toolboxes.sosopt.pcontour3
+
+% system states
+x = casos.Indeterminates('x',4,1);
+u = casos.Indeterminates('u',2,1);
+
+%% Polynomial Dynamics
+f1 = @(V,alpha,q,theta,eta,F) +1.233e-8*V.^4.*q.^2         + 4.853e-9.*alpha.^3.*F.^3    + 3.705e-5*V.^3.*alpha.*q      ...
+                             - 2.184e-6*V.^3.*q.^2         + 2.203e-2*V.^2.*alpha.^3     - 2.836e-6*alpha.^3.*F.^2      ...
+                             + 3.885e-7*alpha.^2.*F.^3     - 1.069e-6*V.^3.*q            - 4.517e-2*V.^2.*alpha.^2      ...
+                             - 2.140e-3*V.^2.*alpha.*eta   - 3.282e-3*V.^2.*alpha.*q     - 8.901e-4*V.^2.*eta.^2        ...
+                             + 9.677e-5*V.^2.*q.^2         - 2.037e-4*alpha.^3.*F        - 2.270e-4*alpha.^2.*F.^2      ...
+                             - 2.912e-8*alpha.*F.^3        + 1.591e-3*V.^2.*alpha        - 4.077e-4*V.^2.*eta           ...
+                             + 9.475e-5*V.^2.*q            - 1.637   *alpha.^3           - 1.631e-2*alpha.^2.*F         ...
+                             + 4.903   *alpha.^2.*theta    - 4.903   *alpha.*theta.^2    + 1.702e-5*alpha.*F.^2         ...
+                             - 7.771e-7*F.^3               + 1.634   *theta.^3           - 4.319e-4*V.^2                ...
+                             - 2.142e-1*alpha.^2           + 1.222e-3*alpha.*F           + 4.541e-4*F.^2                ...
+                             + 9.823   *alpha              + 3.261e-2*F                  - 9.807.*theta      + 4.284e-1;
+
+                      
+% change of angle of attack
+f2 = @(V,alpha,q,theta,eta,F) -3.709e-11*V.^5.*   q.^2     + 6.869e-11*V.*alpha.^3.*F.^3 + 7.957e-10*V.^4.*alpha.*q     ...
+                             + 9.860e-9 *V.^4.*   q.^2     + 1.694e-5 *V.^3.*alpha.^3    - 4.015e-8 *V.*alpha.^3.*F.^2  ...
+                             - 7.722e-12*V.*alpha.^2.*F.^3 - 6.086e-9 *alpha.^3.*F.^3    - 2.013e-8 *V.^4.*q            ...
+                             - 5.180e-5 *V.^3.*alpha.^2    - 2.720e-6 *V.^3.*alpha.*eta  - 1.410e-7 *V.^3.*alpha.*q     ...
+                             + 7.352e-7 *V.^3.*eta.^2      - 8.736e-7 *V.^3.*q.^2        - 1.501e-3 *V.^2.*alpha.^3     ...
+                             - 2.883e-6 *V.*alpha.^3.*F    + 4.513e-9 *V.*alpha.^2.*F.^2 - 4.121e-10*V.*alpha.*F.^3     ...
+                             + 3.557e-6 *alpha.^3.*F.^2    + 6.841e-10*alpha.^2.*F.^3    + 4.151e-5 *V.^3.*alpha        ...
+                             + 3.648e-6 *V.^3.*eta         + 3.566e-6 *V.^3.*q           + 6.246e-6 *V.^2.*alpha.*q     ...
+                             + 4.589e-3 *V.^2.*alpha.^2                                  - 6.514e-5 *V.^2.*eta.^2       ...
+                             + 2.580e-5 *V.^2.*q.^2        - 3.787e-5 *V.*alpha.^3       + 3.241e-7 *V.*alpha.^2.*F     ...
+                             + 2.409e-7 *V.*alpha.*F.^2    + 1.544e-11*V.*F.^3           + 2.554e-4 *alpha.^3.*F        ...
+                             - 3.998e-7 *alpha.^2.*F.^2    + 3.651e-8 *alpha.*F.^3       + 4.716e-7 *V.^3               ...
+                             - 3.677e-3 *V.^2*alpha        - 3.231e-4 *V.^2.*eta         - 1.579e-4 *V.^2.*q            ...
+                             + 2.605e-3 *V.*alpha.^2       + 1.730e-5 *V.*alpha.*F       - 5.201e-3 *V.*alpha.*theta    ...
+                             - 9.026e-9 *V.*F.^2           + 2.601e-3 *V.*theta.^2       + 3.355e-3 *alpha.^3           ...
+                             - 2.872e-5 *alpha.^2.*F       - 2.134e-5 *alpha.*F.^2       - 1.368e-9 *F.^3               ...
+                             - 4.178e-5 *V.^2              + 2.272e-4 *V.*alpha          - 6.483e-7 *V.*F               ...
+                             - 2.308e-1 *alpha.^2          - 1.532e-3 *alpha.*F          + 4.608e-1 *alpha.*theta       ...
+                             - 2.304e-1 *theta.^2          + 7.997e-7 *F.^2              - 5.210e-3 *V                  ...
+                             - 2.013e-2 *alpha             + 5.744e-5 *F          + q    + 4.616e-1;
+                         
+% change of pitch rate                         
+f3 = @(V,alpha,q,theta,eta,F) -6.573e-9*V.^5.*    q.^3     + 1.747e-6*V.^4.*q.^3         - 1.548e-4*V.^3.*q.^3          ...
+                             - 3.569e-3*V.^2.*alpha.^3     + 4.571e-3*V.^2.*q.^3         + 4.953e-5*V.^3.*q             ...
+                             + 9.596e-3*V.^2.*alpha.^2     + 2.049e-2*V.^2.*alpha.*eta   - 2.431e-2*V.^2.*alpha         ...
+                             - 3.063e-2*V.^2.*eta          - 4.388e-3*V.^2.*q            - 2.594e-7*F.^3                ...
+                             + 2.461e-3*V.^2               + 1.516e-4*F.^2               + 1.089e-2*F        + 1.430e-1;                         
+
+
+% change of pitch angle
+f4 = @(V,alpha,q,theta,eta,F)  q;
+
+Kq = 0.0698;
+
+%% Trim condition
+v0      = 45;       % m/s
+alpha0  = 0.04924; % rad
+q0      = 0;       % rad/s
+theta0  = 0.04924; % radg
+
+eta0    = 0.04892; % rad
+delta0  = 14.33;   % percent
+            
+
+x0 = [v0; alpha0; q0; theta0];
+u0 = [eta0; delta0];
+
+% get trim point 
+f = @(x,u) [
+                    f1(x(1,:),x(2,:),x(3,:),x(4,:),u(1,:),u(2,:))
+                    f2(x(1,:),x(2,:),x(3,:),x(4,:),u(1,:),u(2,:))
+                    f3(x(1,:),x(2,:),x(3,:),x(4,:),u(1,:),u(2,:))
+                    f4(x(1,:),x(2,:),x(3,:),x(4,:),u(1,:),u(2,:))
+];
+
+[x0, u0] = findtrim(f,x0, u0);
+
+% set up dynamic system
+f = f(x+x0,u+u0);
+
+d = ([
+          convvel(20, 'm/s', 'm/s')  %range.tas.lebesgue.get('ft/s')
+          convang(20, 'deg', 'rad')  %range.gamma.lebesgue.get('rad')
+          convang(50, 'deg', 'rad')  %range.qhat.lebesgue.get('rad')
+          convang(20, 'deg', 'rad')  %range.alpha.lebesgue.get('rad')
+]);
+
+
+% use scaled dynamics to compute initial guess for lyapunov function
+D = diag(d)^-1;
+
+f = D*subs(f, x, D^-1*x);
+
+f = cleanpoly(f,1e-6,1:5);
+
+% initial guess for control law
+[A,B] = plinearize(f ,x , u);
+[K0,P] = lqr(A,B,eye(4),eye(2));
+
+K = -K0*x;
+
+% initial Lyapunov function
+Vinit = x'*P*x;
+
+% polynomial shape
+p = x'*x*1e2;
+
+% Lyapunov function candidate
+V = casos.PS.sym('v',monomials(x,2));
+
+% SOS multiplier
+s2    = casos.PS.sym('s2',monomials(x,2:4));
+kappa = casos.PS.sym('kappa',monomials(x,1),[2,1]);
+
+% enforce positivity
+l = 1e-6*(x'*x);
+
+% options
+opts = struct('sossol','mosek');
+
+gam = 1;
+
+g = Vinit-2; 
+
+cost = dot(g - (V-gam), g - (V-gam));
+
+
+%% setup solver
+sos = struct('x',[V; s2;kappa],...
+              'f',cost, ...
+              'p',[]);
+
+sos.('g') = [s2; 
+              V-l; 
+              s2*(V-gam)-nabla(V,x)*subs(f,u,kappa)-l];
+
+% states + constraint are SOS cones
+opts.Kx      = struct('lin', length(sos.x));
+opts.Kc      = struct('sos', 3);
+opts.verbose = 1;
+opts.sossol_options.sdpsol_options.error_on_fail = 0;
+
+
+buildTime_in = tic;
+    solver_GTM_syn = casos.nlsossol('S1','sequential',sos,opts);
+buildtime = toc(buildTime_in);
+
+% solve problem
+sol = solver_GTM_syn('x0' ,[Vinit; (x'*x);K]);
+disp(['Solver buildtime: ' num2str(buildtime), ' s'])
+
+
+%% plot solver statistics
+plotSolverStats(solver_GTM_syn.stats);
+
+%% plotting
+import casos.toolboxes.sosopt.pcontour
+
+xD = D*x;
+V = subs(sol.x(1),[x(1);x(4)],zeros(2,1));
+V = subs(V,[x(2);x(3)],xD(2:3));
+
+
+g = subs(g,[x(1);x(4)],zeros(2,1));
+g = subs(g,[x(2);x(3)],xD(2:3));
+
+figure()
+clf
+pcontour(g, 0, [-1 1 -4 4]*2, 'k--');
+hold on
+pcontour(V, gam, [-1 1 -4 4]*2, 'b-');
---
+++ sequential_experiments/ROA_est_examples/sequential_SOS_satellite_rates_constrained.m
@@ -0,0 +1,109 @@
+
+
+import casos.toolboxes.sosopt.cleanpoly
+import casos.toolboxes.sosopt.plinearize
+import casos.toolboxes.sosopt.pcontour
+import casos.toolboxes.sosopt.pcontour3
+
+% system states
+x = casos.Indeterminates('x',3,1);
+u = casos.Indeterminates('u',3,1);
+
+% Casini Parameter
+J = diag([8970;9230;3830]);
+
+% skew-symmetric matrix
+skew = @(x) [   0  -x(3)  x(2); 
+              x(3)   0   -x(1); 
+             -x(2)  x(1)   0 ];
+
+omega_max = 2*pi/180;
+
+% system dynamics
+f = -inv(J)*skew(x(1:3))*J*x(1:3);
+
+gx = inv(J);
+
+[A,B] = plinearize(f + gx*u ,x , u);
+
+[K0,P] = lqr(A,B,eye(3)*0.1,eye(3)*0.01);
+
+K = -K0*x;
+
+
+%% scaled closed-loop dynamics and scaled Lyapunov function
+D = diag([1/(omega_max),1/(omega_max),1/(omega_max)]);
+
+% substitute control law and clean up
+fc = cleanpoly(subs(f + gx*u, u, K), 1e-10);
+
+% scaling
+fc = D*subs(fc,x,D^(-1)*x);
+
+Vinit = (D^(-1)*x)'*P*(D^(-1)*x);
+
+% Lyapunov function candidate
+V = casos.PS.sym('v',monomials(x,2));
+
+% SOS multiplier
+s2 = casos.PS.sym('s2',monomials(x,2));
+s3 = casos.PS.sym('s3',monomials(x,2));
+
+% enforce positivity
+l = 1e-6*(x'*x);
+
+% level of stability
+b = casos.PS.sym('b');
+
+% options
+opts = struct('sossol','mosek');
+
+% pre-computed safe set
+g0 =  673551*x(1)^4 + 673551*x(2)^4 + 673551*x(3)^4 - 0.999933;
+
+% de-scale input
+g0 = subs(g0,x,D^(-1)*x);
+
+pcontour(subs(g0,x(3),0),0,[-omega_max omega_max -omega_max omega_max],'k')
+
+cost = dot(g0 - V, g0 - V) ;
+
+%% setup solver
+sos = struct('x',[V;s2;s3],...
+              'f',cost, ...
+              'p',[]);
+
+
+sos.('g') = [s2;
+              s3; 
+              V - l; 
+              s2*(V - 1) - nabla(V,x)*fc - l;
+              s3*(V - 1) - g0];
+
+% states + constraint are SOS cones
+opts.Kx      = struct('lin', length(sos.x));
+opts.Kc      = struct('sos', length(sos.g));
+opts.verbose = 1;
+opts.sossol_options.sdpsol_options.error_on_fail = 0;
+
+
+buildTime_in = tic;
+    solver_Satellite3D = casos.nlsossol('S','sequential',sos,opts);
+buildtime = toc(buildTime_in);
+
+% solve
+sol = solver_Satellite3D('x0' ,[Vinit; 1;1]);
+disp(['Solver buildtime: ' num2str(buildtime), ' s'])
+
+% plot solver statistics
+plotSolverStats(solver_Satellite3D.stats);
+
+
+% Descale/ scale input
+Vsol = subs(sol.x(1),x,D*x);
+g0   = subs(g0,x,D*x);
+
+figure()
+pcontour(subs(Vsol,x(3),0),1,[-omega_max omega_max -omega_max omega_max],'r')
+hold on
+pcontour(subs(g0,x(3),0),0,[-omega_max omega_max -omega_max omega_max],'k--')
