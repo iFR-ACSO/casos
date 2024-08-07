41f369891f16e067c1a2e0b92f8e3c63ac91f0b0
Renato Loureiro <renato.loureiro@ifr.uni-stuttgart.de>
2024-08-07 10:11:52 +0200

    Small change: pcontour domain and set bisection.

--- demo/pendulum/main.m
+++ demo/pendulum/main.m
@@ -4,7 +4,7 @@
 n = 2;
 
 % degree of the polynomial approximation
-deg = 3;
+deg = 2;
 
 pendulum_dyn_ctrl = ['pendulum_dyn_ctrl_n' num2str(n)];
 pendulum_dyn_poly = ['pendulum_dyn_poly_n' num2str(n) '_d' num2str(deg)];
@@ -65,7 +65,6 @@ p = x'*x;
 
 % scalar decision variable
 g = casos.PS.sym('g');
-g = 0.24;
 
 % multiplier (idk)
 s = casos.PS.sym('s',monomials(x,0:2));
@@ -74,15 +73,14 @@ s = casos.PS.sym('s',monomials(x,0:2));
 sos = struct('x',s,'f',-g,'g', s*(V-g)-nabla(V,x)*f);
 
 % options
-%opts = struct('sossol','mosek');
+opts = struct('sossol','mosek');
 
 % constraint is scalar SOS cone
-opts.Kx = struct('dsos', 1);
+opts.Kx = struct('sdsos', 1);
 opts.Kc = struct('sos', 1);
 
 % solve by relaxation to SDP
-S1 = casos.sossol('S1', 'mosek', sos, opts);
-%S1 = casos.qcsossol('S1','bisection',sos,opts);
+S1 = casos.qcsossol('S1','bisection',sos,opts);
 
 % evaluate
 tic
@@ -96,7 +94,7 @@ import casos.toolboxes.to_multipoly
 figure(1)
 V_2d = subs(V, [x(2:n); x(n+2:2*n)], zeros(2*n-2,1));
 level = full(-sol.f(1));
-pcontour(V_2d, level, [-5 5 -5 5])
+pcontour(V_2d, level, [-5 5 -12 12])
 grid 
 grid minor
 
