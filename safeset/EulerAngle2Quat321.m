7e5dcb80e72f0004233f64ea9f1e9daf400f278f
JOlCode <61011693+JOlCode@users.noreply.github.com>
2024-08-08 15:49:21 +0200

    delete unncessary trial files

--- safeset/EulerAngle2Quat321.m
+++
@@ -1,9 +0,0 @@
-function [q] = EulerAngle2Quat321(roll,pitch,yaw)
-
-        q = [cos(roll/2)*cos(pitch/2)*cos(yaw/2) + sin(roll/2)*sin(pitch/2)*sin(yaw/2);
-            -cos(roll/2)*sin(pitch/2)*sin(yaw/2) + cos(pitch/2)*cos(yaw/2)*sin(roll/2);
-             cos(roll/2)*cos(yaw/2)*sin(pitch/2) + sin(roll/2)*cos(pitch/2)*sin(yaw/2);
-             cos(roll/2)*cos(pitch/2)*sin(yaw/2) - sin(roll/2)*cos(yaw/2)*sin(pitch/2)];
-
-end
-
--- safeset/skew.m
+++
@@ -1,4 +0,0 @@
-function vMat = skew(v) 
-
-vMat = [0 -v(3) v(2) ; v(3) 0 -v(1) ; -v(2) v(1) 0];
-end
