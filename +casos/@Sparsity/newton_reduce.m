a0a54407b67d54e4cd7e9ffae9b62e4840cea0ea
Renato Loureiro <renato.loureiro@ifr.uni-stuttgart.de>
2024-09-03 17:00:56 +0200

    Newton polytope enhancements: 1. remove nested if statements inside the for loop. 2. remove multiple transposes that are not necessary to do 3. change name of keep to keep_trivial, since these are the one that were easily obtained.

--- +casos/@Sparsity/newton_reduce.m
+++ +casos/@Sparsity/newton_reduce.m
@@ -1,5 +1,7 @@
 function Lz = newton_reduce(obj, Pdegmat, Zdegmat)
-% removes monomials outside half Newton polytope
+% Removes monomials outside half Newton polytope
+% Strategy inpired from: Simplification Methods for Sum-of-Squares 
+% Programs, Peter Seiler et al.
 
 % options for linprog
 options = optimoptions('linprog');
@@ -9,39 +11,41 @@ options.Display = 'off';
 bfixed = [zeros(size(Pdegmat,1),1); 1];
 
 % trivial removal of monomials
-keep = ismember(Zdegmat*2,Pdegmat,'rows');
-dontkeep = false(size(keep));
+keep_trivial = ismember(Zdegmat*2,Pdegmat,'rows');
+dontkeep = false(size(keep_trivial));
 
-% try to go over each possible monomial basis and verify of it belongs to
+% try to go over each possible monomial basis and verify if it belongs to
 % the newton polytope by checking for the existance of a hyperplane 
-for i = 1:length(keep)
+for i = 1:length(keep_trivial)
+
+    if keep_trivial(i) || dontkeep(i)
+        continue;
+    end
     
-    if ~keep(i) && ~dontkeep(i)
-        % build LP (part 2)
-        q = Zdegmat(i,:)'*2;
-        c = ([-q' 1])';
-        F_struc = ([bfixed -[Pdegmat -ones(size(Pdegmat,1),1); q' -1]]);
-        
-        A = -F_struc(1:end,2:end);
-        b = F_struc(1:end,1);   
+    % build LP (part 2)
+    q = Zdegmat(i,:)*2;
+    c = ([-q 1])';
+    F_struc = ([bfixed -[Pdegmat -ones(size(Pdegmat,1),1); q -1]]);
     
-        % solve LP
-        [x,~,flag] = linprog(c, A, b, [], [], [], [], options);
-        
-        % in case the LP is not feasible, giving an empty output 'x' 
-        if isempty(x)
-            x = zeros(length(c),1);
-        end
-        
-        % If the LP gives an unbounded solution or the only solution is a
-        % zero vector
-        if (flag > 0 && ([-q' 1]*x(:) < 0)) || flag == -3
-            a = x(1:end-1);
-            b = x(end);
-            u = a'*2*Zdegmat' - b > sqrt(eps);
-            dontkeep(u) = 1;
-        end
-    end  
+    A = -F_struc(1:end,2:end);
+    b = F_struc(1:end,1);   
+
+    % solve LP
+    [x,~,flag] = linprog(c, A, b, [], [], [], [], options);
+    
+    % in case the LP is not feasible, giving an empty output 'x' 
+    if isempty(x)
+        x = zeros(length(c),1);
+    end
+    
+    % If the LP gives an unbounded solution or the only solution is a
+    % zero vector
+    if (flag > 0 && ([-q 1]*x(:) < 0)) || flag == -3
+        a = x(1:end-1);
+        b = x(end);
+        u = 2*Zdegmat*a - b > sqrt(eps);
+        dontkeep(u) = 1;
+    end
 end
 
 Lz = ~dontkeep;
