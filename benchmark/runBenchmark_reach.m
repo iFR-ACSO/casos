e6c0f34c6c672cc640aa366e9b0f5fac09e82bd7
JOlCode <61011693+JOlCode@users.noreply.github.com>
2024-09-25 17:34:06 +0200

    update benchmark scirpt of reachability problem

--- benchmark/runBenchmark_reach.m
+++ benchmark/runBenchmark_reach.m
@@ -18,7 +18,7 @@ addpath(genpath('./otherFrameworks'))
 % keep the last gamma value and the last storage function for later volume
 % computation
 
-noRuns = 5;
+noRuns = 1;
 
 % pre-allocate
 solverTime_total_c_GTM_arr = zeros(noRuns,1);
@@ -34,7 +34,7 @@ buildTime_sp_GTM_arr        = zeros(noRuns,1);
 solverTime_total_sopt_GTM_arr = zeros(noRuns,1);
 buildTime_sopt_GTM_arr        = zeros(noRuns,1);
 
-for j = 1:1
+for j = 1:noRuns
 
  disp(['Round number: ' num2str(j)])
 %% run GTM 4D example in casos
@@ -86,30 +86,30 @@ end
 % save latest benchmark
 save('Benchmark_gtm_reach.mat')
 
-%%
+%% get mean computation times
 % from the five runs, take the mean value
-buildTime_c_GTM     = mean(buildTime_c_GTM_arr)
+buildTime_c_GTM            = mean(buildTime_c_GTM_arr)
 solverTime_total_c_GTM     = mean(solverTime_total_c_GTM_arr)
-total_c = buildTime_c_GTM + solverTime_total_c_GTM
+total_c                    = buildTime_c_GTM + solverTime_total_c_GTM
 
-buildTime_st_GTM    = mean(buildTime_st_GTM_arr)
+buildTime_st_GTM           = mean(buildTime_st_GTM_arr)
 solverTime_total_st_GTM    = mean(solverTime_total_st_GTM_arr)
-total_st = buildTime_st_GTM + solverTime_total_st_GTM
+total_st                   = buildTime_st_GTM + solverTime_total_st_GTM
 
-buildTime_sp_GTM    = mean(buildTime_sp_GTM_arr)
+buildTime_sp_GTM           = mean(buildTime_sp_GTM_arr)
 solverTime_total_sp_GTM    = mean(solverTime_total_sp_GTM_arr)
-total_sp = buildTime_sp_GTM + solverTime_total_sp_GTM
+total_sp                   = buildTime_sp_GTM + solverTime_total_sp_GTM
 
-buildTime_sopt_GTM  = mean(buildTime_sopt_GTM_arr)
+buildTime_sopt_GTM         = mean(buildTime_sopt_GTM_arr)
 solverTime_total_sopt_GTM  = mean(solverTime_total_sopt_GTM_arr)
-total_sopt = buildTime_sopt_GTM + solverTime_total_sopt_GTM
+total_sopt                 = buildTime_sopt_GTM + solverTime_total_sopt_GTM
 
 
 %% plot result
-solvers = {'Ca\SigmaoS' ,'SOSTOOLS' , 'SPOTless','SOSOPT'};
+% solvers = {'Ca\SigmaoS' ,'SOSTOOLS' , 'SPOTless','SOSOPT'};
 solvers = {'A' ,'B' , 'C','D'};
-buildTimes  = [buildTime_c_GTM,buildTime_st_GTM,buildTime_sp_GTM,buildTime_sopt_GTM];                 % Time spent building for each solver
-solverTimes = [solverTime_total_c_GTM,solverTime_total_st_GTM,solverTime_total_sp_GTM,solverTime_total_sopt_GTM]; % Time spend in low-level solver 
+buildTimes  = [buildTime_c_GTM, buildTime_st_GTM, buildTime_sp_GTM, buildTime_sopt_GTM];                             % Time spent building for each solver
+solverTimes = [solverTime_total_c_GTM, solverTime_total_st_GTM, solverTime_total_sp_GTM, solverTime_total_sopt_GTM]; % Time spend in low-level solver 
 
 % Combine the build and solve times into a matrix
 timeData = [buildTimes; solverTimes]';
@@ -132,15 +132,6 @@ grid on;
 % Calculate the total times for each solver (buildTime + solverTime)
 totalTimes = sum(timeData, 2);
 
-% Add the total times as text labels at the top of each stack
-x = 1:length(solvers); % X positions for each bar
-
-% Loop through each bar to place the text
-% for i = 1:length(x)-1
-%     % The position is above the top of each stack (build + solve)
-%     text(x(i), totalTimes(i), sprintf('%.2f s', totalTimes(i)), ...
-%          'VerticalAlignment', 'bottom', 'HorizontalAlignment', 'center');
-% end
 
 axis([0.5 length(solvers)+0.5 0 max(totalTimes)+0.1*max(totalTimes)])
 % Add a legend to indicate what each part of the stack represents
@@ -150,51 +141,50 @@ legend('Parsing/Build Time', 'Solver Time','Location', 'northwest');
 %% estimate volume via Monte-Carlo-Sampling (plotting of sublevel set)
 import casos.toolboxes.to_multipoly
 
-%casos
+% casos
 Vc_indet = V_c.indeterminates;
-V0_c = subs(V_c,Vc_indet(1),0);
+V0_c = subs(V_c,Vc_indet(1),0); % V(0,x)
 
-% we use multipoly function to compute the volume
-V_c_mult =  to_multipoly(V0_c-gval_c_GTM);
+% we use multipoly function to compute the volume also for CaSoS
+V_c_mult =  to_multipoly(V0_c - gval_c_GTM);
+
+[vol_c,std_c]       = pvolume(V_c_mult,0)
 
 % sostools and sosopt use both multipoly
-% System dynamics
-pvar x1 x2 x3 x4 t
-[vol_st,std_st]     = pvolume(subs(V_st,t,0)-gval_st_GTM,0)
+pvar x1 x2 x3 x4 t % for plotting
+[vol_st,std_st]     = pvolume(subs(V_st,t,0)-gval_st_GTM,0) % sostools
 
-pvar x_1 x_2 x_3 x_4
-[vol_sopt,std_sopt] = pvolume(subs(V_sopt,t,0)-gval_sopt_GTM,0)
+pvar x_1 x_2 x_3 x_4 % for plotting
+[vol_sopt,std_sopt] = pvolume(subs(V_sopt,t,0)-gval_sopt_GTM,0) % sosopt
 
-[vol_c,std_c] = pvolume(subs(V_c_mult,t,0),0)
+% spotless
+x = msspoly ('x' , 4 ) ; % for plotting
+t = msspoly ('t' , 1 ) ;
+V0_sp = subs(V_sp,t,0);
 
-% plot sublevel sets
+[vol_sp,std_sp] = pvolume_spot(V0_sp,x,gval_sp_GTM)
+
+%% plot sublevel sets i.e. slice of x1x2
 figure(3)
 pcontour(subs(subs(V_sopt,[x_3;x_4],[0;0]),t,0)-gval_sopt_GTM,0,[-5 5 -5 5]./5,'b')
 hold on
 pcontour(subs(V_c_mult,[x_3;x_4],[0;0]),0,[-5 5 -5 5]./5,'r')
 pcontour(subs(subs(V_st,[x3;x4],[0;0]),t,0)-gval_st_GTM,0,[-5 5 -5 5]./5,'g')
-    
+
+% plot spotless solution
 domain = [-5 5 -5 5]./5;
 xg      = linspace(domain(1),domain(2),100);
 yg      = linspace(domain(3),domain(4),100);
 [xg,yg] = meshgrid(xg,yg);
-x = msspoly ('x' , 4 ) ;
-t = msspoly ('t' , 1 ) ;
-V0_sp = subs(V_sp,t,0);
-
-[vol_c,std_c] = pvolume_spot(V0_sp,x,gval_sp_GTM)
 
 V0_sp = subs(V0_sp,x(3:4),[0;0]);
 pgrid = double(msubs(V0_sp,x(1:2),[xg(:)'; yg(:)']));
 pgrid = reshape(pgrid,size(xg));
 contour(xg,yg,double(pgrid),[0,0],'k');
-
-
-
 grid minor
 legend('SOSOPT','CaSoS','SOSTOOLS','SPOTless')
 
 
 %% make tikz figure
-cleanfigure()
-matlab2tikz('benchmark_gtm_reach.tex','width','\figW','height','\figH');
+% cleanfigure()
+% matlab2tikz('benchmark_gtm_reach.tex','width','\figW','height','\figH');
