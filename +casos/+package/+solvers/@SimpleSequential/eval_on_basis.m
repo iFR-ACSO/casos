ab422b67700d023a467458d9a7d6cb8ffe3e48e3
JOlCode <61011693+JOlCode@users.noreply.github.com>
2024-12-06 17:09:53 +0100

    put code into dedicated functions (better for readability and maintanence

--- +casos/+package/+solvers/@SimpleSequential/eval_on_basis.m
+++ +casos/+package/+solvers/@SimpleSequential/eval_on_basis.m
@@ -104,36 +104,8 @@ while iter <= obj.opts.max_iter
         break
     else
 
-       % update BFGS
-       s_val = full( obj.eval_s(x_k,sol_iter.x_k1,p0) );
-       y_val = full( obj.eval_y(x_k,sol_iter.x_k1,p0,sol_iter.dual_k1) );
-        
-        if s_val'*Bk*s_val < eps || s_val'*y_val < eps
-            % skip
-        else
-
-            % Powell damping
-            if s_val'*y_val >= 0.2*s_val'*Bk*s_val 
-                theta = 1;
-            else
-                theta = (0.8*s_val'*Bk*s_val)/(s_val'*Bk*s_val-s_val'*y_val);
-            end
-           
-            r_val = full( obj.eval_r(Bk,theta,y_val,s_val) );
-            
-            % B = B + (r*r')/(s'*r) - (B*s*s'*B)/(s'*B*s);
-            Bk =  full( obj.damped_BFGS(Bk,r_val,s_val) );
-            
-            % check symmetry of Hessian and ajdust if necessary
-            Bknonsym = norm(Bk-Bk',inf);
-
-            if Bknonsym > eps
-                Bk = (Bk+Bk')/2;
-            end
-
-        end
-
-        
+       Bk = damped_BFGS(obj,Bk,x_k,p0,sol_iter);
+    
        delta_xi_double   = norm(full( sol_iter.x_k1 - x_k) ,inf);
        delta_dual_double = norm(full( sol_iter.dual_k1 - dual_k ),inf);
 
@@ -146,16 +118,15 @@ while iter <= obj.opts.max_iter
        iter = iter + 1;
     end
 
-% return last solution
-sol =sol_qp;
+    % return last solution
+    sol =sol_qp;
 
-sol{1} = sol_iter.x_k1;
-sol{5} = sol_iter.dual_k1;
-
-
+    sol{1} = sol_iter.x_k1;
+    sol{5} = sol_iter.dual_k1;
 
 end % end of while loop
 
+% assign output
 argout = sol;
 
 % store iteration info
