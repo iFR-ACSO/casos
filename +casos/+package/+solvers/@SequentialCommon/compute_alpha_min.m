2a7e1bab2356bd3658db3fae1c45049318675ec2
JOlCode <61011693+JOlCode@users.noreply.github.com>
2024-12-18 05:25:08 +0100

    added minimum step length computation after Biegler/Waechter

---
+++ +casos/+package/+solvers/@SequentialCommon/compute_alpha_min.m
@@ -0,0 +1,15 @@
+function alpha_min = compute_alpha_min(obj,x_k,dk,p0,s_phi,delta,theta_xk,s_theta,gamma_theta,gamma_phi,gamma_alpha)
+
+% descent direction
+nabla_f_dir = full(obj.eval_gradCost(x_k,p0)*dk);
+
+if nabla_f_dir  < 0 
+
+   alpha_min = gamma_alpha*min([gamma_theta, (gamma_phi*theta_xk)/(-nabla_f_dir), (delta*theta_xk^s_theta)/((-nabla_f_dir)^s_phi) ]);
+
+else
+    
+   alpha_min = gamma_alpha*gamma_theta;
+
+end
+
