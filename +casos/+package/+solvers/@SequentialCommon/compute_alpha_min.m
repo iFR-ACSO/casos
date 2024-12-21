4d9b621fdc6fef9612e6c6c24e84c88edf39f6c6
JOlCode <61011693+JOlCode@users.noreply.github.com>
2024-12-21 16:20:45 +0100

    update of BFGS and Q-SDP

--- +casos/+package/+solvers/@SequentialCommon/compute_alpha_min.m
+++ +casos/+package/+solvers/@SequentialCommon/compute_alpha_min.m
@@ -3,6 +3,7 @@ function alpha_min = compute_alpha_min(obj,x_k,dk,p0,s_phi,delta,theta_xk,s_thet
 % descent direction
 nabla_f_dir = full(obj.eval_gradCost(x_k,p0)*dk);
 
+% theta_xk can be zero. in that case
 if nabla_f_dir  < 0 
 
    alpha_min = gamma_alpha*min([gamma_theta, (gamma_phi*theta_xk)/(-nabla_f_dir), (delta*theta_xk^s_theta)/((-nabla_f_dir)^s_phi) ]);
--- +casos/+package/+solvers/@SequentialCommon/damped_BFGS.m
+++ +casos/+package/+solvers/@SequentialCommon/damped_BFGS.m
@@ -1,12 +1,9 @@
-function Bk = damped_BFGS(obj,Bk,x_k,p0,sol_iter)
+function Bk = damped_BFGS(obj,Bk,x_k,p0,sol_iter,iter)
 
        % update BFGS
        s_val = full( obj.eval_s(x_k,sol_iter.x_k1,p0) );
        y_val = full( obj.eval_y(x_k,sol_iter.x_k1,p0,sol_iter.dual_k1) );
         
-        if s_val'*Bk*s_val < eps || s_val'*y_val < eps
-            % skip
-        else
 
             % Powell damping
             if s_val'*y_val >= 0.2*s_val'*Bk*s_val 
@@ -16,7 +13,8 @@ function Bk = damped_BFGS(obj,Bk,x_k,p0,sol_iter)
             end
            
             r_val = full( obj.eval_r(Bk,theta,y_val,s_val) );
-            
+
+      
             % B = B + (r*r')/(s'*r) - (B*s*s'*B)/(s'*B*s);
             Bk =  full( obj.damped_BFGS(Bk,r_val,s_val) );
             
@@ -26,5 +24,13 @@ function Bk = damped_BFGS(obj,Bk,x_k,p0,sol_iter)
             if Bknonsym > eps
                 Bk = (Bk+Bk')/2;
             end
-        end
+
+            if min(eig(Bk)) <= 0
+                Bk = Bk + eye(size(Bk))*sqrt(eps);
+            end
+            
+            
+            
+              
+       
 end
