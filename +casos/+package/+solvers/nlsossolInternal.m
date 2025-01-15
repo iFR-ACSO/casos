997c3b548004036dfe586dab85efd63bedc67db0
JOlCode <61011693+JOlCode@users.noreply.github.com>
2025-01-15 10:42:15 +0100

    renamed simple-sequential to filter linesearch

--- +casos/+package/+solvers/nlsossolInternal.m
+++ +casos/+package/+solvers/nlsossolInternal.m
@@ -2,8 +2,8 @@ function node = nlsossolInternal(name,solver,varargin)
 % Internal interface for noncovex sum-of-squares problems.
 
 switch (solver)
-    case 'sequential'
-        node = casos.package.solvers.SimpleSequential(name,varargin{:});
+    case 'filter-linesearch'
+        node = casos.package.solvers.FilterLinesearch(name,varargin{:});
     otherwise
         error('No such nonlinear solver "%s".',solver)
 end
