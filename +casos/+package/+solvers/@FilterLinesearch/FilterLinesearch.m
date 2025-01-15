db15ab5b4583ac3ebe7eb3e7fb5d518e88bb26cc
JOlCode <61011693+JOlCode@users.noreply.github.com>
2025-01-15 10:42:15 +0100

    renamed simple-sequential to filter linesearch

---
+++ +casos/+package/+solvers/@FilterLinesearch/FilterLinesearch.m
@@ -0,0 +1,95 @@
+classdef FilterLinesearch < casos.package.solvers.SequentialCommon
+% A simple sequential sum-of-squares algorithm.
+
+properties (SetAccess=private)
+    class_name = 'FilterLinesearch';
+end
+
+properties(Access=protected)
+    feas_res_solver
+end
+
+methods (Access=protected)
+    % iteration for overloading
+    varargout = run_iteration(varargin);
+    varargout = do_single_iteration(varargin);
+
+    % internal evaluation
+    argout = eval_on_basis(obj,argin);
+end
+
+methods
+    % Constructor
+    function obj = FilterLinesearch(name,nlsos,varargin)
+        obj@casos.package.solvers.SequentialCommon(name,nlsos,varargin{:});
+
+        % states
+        if ~isfield(nlsos,'x')
+            error('No decision variables given.')
+        else
+            nlsos.x = casos.PS(nlsos.x);
+        end
+        % parameter
+        if ~isfield(nlsos,'p')
+            % not parametrized
+            nlsos.p = casos.PS;
+        else
+            nlsos.p = casos.PS(nlsos.p);
+        end
+        % objective
+        if ~isfield(nlsos,'f')
+            nlsos.f = casos.PS(0);
+        else
+            nlsos.f = casos.PS(nlsos.f);
+        end
+        % constraints
+        if ~isfield(nlsos,'g')
+            nlsos.g = casos.PS;
+        else
+            nlsos.g = casos.PS(nlsos.g);
+        end
+
+        %% set up feasibility restoration phase --> do we need all or only nonlinear
+        
+
+        base_g = sparsity(nlsos.g);
+        base_x = sparsity(nlsos.x);
+        
+        r  = casos.PS.sym('r',length(nlsos.g));
+        s0 = casos.PD(base_g,ones(base_g.nnz,1));
+        
+        nlsos_feas.x = [r;nlsos.x];
+        
+        nlsos_feas.g = nlsos.g + r.*s0;
+        
+        x_R   = casos.PS.sym('x_R',base_x);
+        nlsos_feas.f = sum(r) + 0.1/2*dot(nlsos.x-x_R,nlsos.x-x_R);
+        
+        nlsos_feas.p = [nlsos.p; x_R];
+        
+        obj.FeasRes_para.n_r = length(nlsos.g);
+        
+        
+        sosopt = obj.opts.sossol_options;
+        
+        sosopt.Kx.lin = length(nlsos_feas.x);
+        sosopt.Kc.sos = length(nlsos_feas.g);
+        sosopt.error_on_fail = false;
+        sosopt.verbose = 1;
+        sosopt.max_iter = 100;
+        obj.feas_res_solver  =  casos.package.solvers.FeasibilityRestoration('feasRes',nlsos_feas,sosopt);
+
+    end
+
+    function s = get_stats(obj)
+        % Return stats.
+        s = obj.info;
+        s = addfield(obj.status,s);
+    end
+
+end
+end
+
+
+
+
---
+++ +casos/+package/+solvers/@FilterLinesearch/eval_on_basis.m
@@ -0,0 +1,289 @@
+function argout = eval_on_basis(obj,argin)
+% Solve sequential SOS problem.
+
+import casos.package.UnifiedReturnStatus
+
+%% print output with current problem and setting
+printf(obj.log,'debug','------------------------------------------------------------------------------------------\n');
+printf(obj.log,'debug',['\t\t Ca' char(931) 'oS-Nonlinear sum-of-squares optimization suite \n']);
+printf(obj.log,'debug','\t\t Sequential Quadratic Sum-of-Squares Solver v1.0, December''24 \n');
+printf(obj.log,'debug','\t\t GNU GENERAL PUBLIC LICENSE \n');
+printf(obj.log,'debug','\t\t Institute of Flight Mechanics and Control, Univ. of Stuttgart \n');
+printf(obj.log,'debug','------------------------------------------------------------------------------------------\n');
+printf(obj.log,'debug','Problem:\n Decision variables = %d (coefficents) \n SOS constraints \t= %d \n Linear constraints = %d\n',obj.display_para.no_decVar   , obj.display_para.no_sosCon, 0 );
+printf(obj.log,'debug','Settings:\n Solver:\t%s\n max_iter:\t%d \n Con. Violation Check: \t%s\n', obj.display_para.solver,obj.opts.max_iter,'signed-distance');
+printf(obj.log,'debug','------------------------------------------------------------------------------------------\n');
+printf(obj.log,'debug','%-8s%-15s%-15s%-15s%-15s%-10s%-10s\n', 'iter', 'obj', ['||' char(916) 'x'  '||_inf'], ['||' char(916) char(955)  '||_inf'], [char(920) '(x_k)'],char(945),'||dLdx||');
+printf(obj.log,'debug','------------------------------------------------------------------------------------------\n');
+
+% input arguments
+args = argin;
+
+% initial guess (user)
+x_k = args{1};
+
+% parameter from nonlinear problem
+p0   = args{2};
+
+% initialize iteration info struct
+info = cell(1,obj.opts.max_iter);
+
+        
+% initialize BFGS-matrix; identity times scaling
+if strcmp(obj.opts.Hessian_init,'Identity')
+
+    Bk =  eye(obj.init_para.size_B)*obj.opts.scale_BFGS0;
+
+elseif strcmp(obj.opts.Hessian_init,'Analytical')
+
+    dual_k = zeros(obj.init_para.no_dual_var,1);
+
+    H = full(obj.hess_fun(x_k,p0,dual_k));
+
+    Bk = hessian_regularization(H);
+
+    
+end
+
+
+% initialize filter and first iterate 
+args_conVio     =  args;
+args_conVio{2}  =  [p0; x_k];
+args_conVio{3}  = -inf(obj.init_para.conVio.no_con,1);
+args_conVio{4}  =  inf(obj.init_para.conVio.no_con,1);
+
+sol_convio = eval_on_basis(obj.solver_conVio, args_conVio);
+
+theta_x_k = full(max(0,max(sol_convio{1})));
+
+f_x_k     = full(obj.eval_cost(x_k,p0));
+
+L_k = full(obj.L(x_k,p0,dual_k));
+
+
+filter = [max(1,theta_x_k)*10, inf];
+
+iter = 1;
+
+% just for display output in first iteration
+delta_xi_double   = norm(full( casadi.DM(x_k)),inf);
+delta_dual_double = norm(full( (dual_k)),inf);
+alpha_k           = 1;
+
+feasibility_flag = 1;
+
+measTime_seqSOS_in = tic;
+while iter <= obj.opts.max_iter
+    
+    % display output 
+    if ~mod(iter,10) && iter > 0
+        printf(obj.log,'debug','%-8s%-15s%-15s%-15s%-15s%-10s%-10s\n', 'iter', 'obj', ['||' char(916) 'x'  '||_inf'], ['||' char(916) char(955)  '||_inf'], [char(920) '(x_k)'],char(945),'||dLdx||');
+        printf(obj.log,'debug','------------------------------------------------------------------------------------------\n');
+    end
+    
+
+     % scale = max(100,norm(full(dual_k),1)/length(dual_k))/100;
+
+    printf(obj.log,'debug','%-8d%-15e%-15e%-15e%-15e%-10f%-10e\n',...
+            iter, f_x_k , delta_xi_double, delta_dual_double, theta_x_k , alpha_k , full(casadi.DM( full(obj.eval_gradLang(x_k,p0,dual_k)) ))   );
+        
+    %% check convergence (first-order optimality)
+    if full(obj.eval_gradLang(x_k,p0,dual_k))  <= obj.opts.tolerance_opt*max(1,full(obj.eval_gradLang(x_k,p0,dual_k))) ...
+        && theta_x_k <= obj.opts.tolerance_con && feasibility_flag == 0
+       
+        printf(obj.log,'debug','------------------------------------------------------------------------------------------\n');
+        printf(obj.log,'debug','Solution status: Optimal solution found\n');
+        solveTime = toc(measTime_seqSOS_in);
+        printf(obj.log,'debug',['Solution time: ' num2str(solveTime) ' s\n']);
+        printf(obj.log,'debug',['Build time: ' num2str(obj.display_para.solver_build_time) ' s\n']);
+        printf(obj.log,'debug',['Total time: ' num2str(obj.display_para.solver_build_time+solveTime) ' s\n']);
+        feasibility_flag = 1;
+
+        break
+
+    end
+    
+    feasibility_flag = 0;
+
+    % compute solution of current iterate: solve Q-SDP, perform linesearch and update BFGS
+    [sol_iter,sol_qp,feas_res_flag,info,obj,filter,Bk] = do_single_iteration(obj, ...
+                                                                             iter,...
+                                                                             x_k,...
+                                                                             dual_k,...
+                                                                             theta_x_k,...
+                                                                             f_x_k,...
+                                                                             Bk,...
+                                                                             p0,...
+                                                                             args, ...
+                                                                             filter, ...
+                                                                             info);
+
+                
+    % Invoke feasibility restoration if necessary
+    if feas_res_flag >= 1 && ...              % restoration requested
+       theta_x_k >  obj.opts.tolerance_con    % is the constraint violation larger then tolerance; otherwise restoration does not make sense
+        
+        % augment filter with the iterate at which we invoke restoration
+        gamma_theta = obj.opts.filter_struct.gamma_theta ;
+        gamma_phi   = obj.opts.filter_struct.gamma_phi ;
+
+        % augment filter with iterate where feas. res invoked
+        filter = [filter;[theta_x_k*(1-gamma_theta), f_x_k - gamma_phi*theta_x_k]];
+        
+        % feasibility restoration phase
+        [restored_sol,~,filter,feasibility_flag] = obj.feas_res_solver.eval_extended(filter,x_k,theta_x_k,p0,obj);
+
+        if feasibility_flag >=1
+           
+            
+           % continue from restored solution
+           delta_xi_double   = norm(full( restored_sol.x_k1 - x_k) ,inf);
+           delta_dual_double = norm(full( restored_sol.dual_k1 - dual_k ),inf);
+
+           x_k       = restored_sol.x_k1;
+           dual_k    = restored_sol.dual_k1;
+           theta_x_k = restored_sol.theta_x_k1;
+           f_x_k     = restored_sol.f_x_k1;
+           alpha_k   = restored_sol.alpha_k;
+            
+           
+      
+        else
+            % could not recover return iterate where restoration was
+            % invoked
+            sol    = sol_qp;
+        
+            sol{1} = x_k;
+            sol{5} = dual_k;
+
+            break
+        end
+        
+    elseif feas_res_flag >= 1 &&  theta_x_k < obj.opts.tolerance_con
+            % we are already better then treshhold i.e. restoration can't
+            % make it better
+            sol    = sol_qp;
+
+            % if one, means Q-SDP is infeasible, thus sol.iter should be
+            % zero hence return "old" iterate
+            if feas_res_flag == 1
+                sol{1} = x_k;
+                sol{5} = dual_k;
+            else
+                sol{1} = sol_iter.x_k1;
+                sol{5} = sol_iter.dual_k1;
+            end
+
+                if full(obj.eval_gradLang(x_k,p0,dual_k))  <= obj.opts.tolerance_opt*max(1,full(obj.eval_gradLang(x_k,p0,dual_k))) ...
+                    && theta_x_k <= obj.opts.tolerance_con 
+                        
+                            printf(obj.log,'debug','------------------------------------------------------------------------------------------\n');
+                            printf(obj.log,'debug','Solution status: Optimal solution found\n');
+                            solveTime = toc(measTime_seqSOS_in);
+                            printf(obj.log,'debug',['Solution time: ' num2str(solveTime) ' s\n']);
+                            printf(obj.log,'debug',['Build time: ' num2str(obj.display_para.solver_build_time) ' s\n']);
+                            printf(obj.log,'debug',['Total time: ' num2str(obj.display_para.solver_build_time+solveTime) ' s\n']);
+                            feasibility_flag = 1;
+
+                else
+                    % stalled
+                feasibility_flag = -2;
+
+                end
+
+            break
+
+
+
+    else
+
+       % for display output
+       delta_xi_double   = norm(full( sol_iter.x_k1 - x_k) ,inf);
+       delta_dual_double = norm(full( sol_iter.dual_qp - dual_k ),inf);
+
+       x_k       = sol_iter.x_k1;
+       dual_k    = sol_iter.dual_qp;
+       theta_x_k = sol_iter.theta_x_k1;
+       f_x_k     = sol_iter.f_x_k1;
+       alpha_k   = sol_iter.alpha_k;
+    
+
+    end
+
+    % return last solution
+    sol    = sol_qp;
+    
+    % exchange the primal/dual solution of QP with the accepted iterate
+    sol{1} = sol_iter.x_k1;
+    sol{2} = sol_iter.f_x_k1;
+    sol{5} = sol_iter.dual_k1;
+
+
+    iter = iter + 1;
+
+end % end of while loop
+
+% display output for user
+if iter >= obj.opts.max_iter
+
+        printf(obj.log,'debug','------------------------------------------------------------------------------------------\n');
+        printf(obj.log,'debug','Solution status: Maximum number of iterations reached\n');
+        solveTime = toc(measTime_seqSOS_in);
+        printf(obj.log,'debug',['Solution time: ' num2str(solveTime) ' s\n']);
+        printf(obj.log,'debug',['Build time: ' num2str(obj.display_para.solver_build_time) ' s\n']);
+        printf(obj.log,'debug',['Total time: ' num2str(obj.display_para.solver_build_time+solveTime) ' s\n']);  
+
+end
+
+if feasibility_flag == 0
+        
+    % print last iterate
+        printf(obj.log,'debug','%-8d%-15e%-15e%-15e%-15e%-10f%-10e\n',...
+            iter, f_x_k , delta_xi_double, delta_dual_double, theta_x_k , alpha_k , full(casadi.DM( full(obj.eval_gradLang(x_k,p0,dual_k)) ) )   );
+        
+        printf(obj.log,'debug','------------------------------------------------------------------------------------------\n');
+        printf(obj.log,'debug','Solution status: Problem infeasible.\n');
+        solveTime = toc(measTime_seqSOS_in);
+        printf(obj.log,'debug',['Solution time: ' num2str(solveTime) ' s\n']);
+        printf(obj.log,'debug',['Build time: ' num2str(obj.display_para.solver_build_time) ' s\n']);
+        printf(obj.log,'debug',['Total time: ' num2str(obj.display_para.solver_build_time+solveTime) ' s\n']);  
+
+elseif feasibility_flag == -1
+
+
+        % print last iterate
+        printf(obj.log,'debug','%-8d%-15e%-15e%-15e%-15e%-10f%-10e\n',...
+            iter, f_x_k , delta_xi_double, delta_dual_double, theta_x_k , alpha_k , full(casadi.DM( full(obj.eval_gradLang(x_k,p0,dual_k)) ) )   );
+        
+
+        printf(obj.log,'debug','------------------------------------------------------------------------------------------\n');
+        printf(obj.log,'debug','Solution status: Solver stalled in feasibility restoration.\n');
+        solveTime = toc(measTime_seqSOS_in);
+        printf(obj.log,'debug',['Solution time: ' num2str(solveTime) ' s\n']);
+        printf(obj.log,'debug',['Build time: ' num2str(obj.display_para.solver_build_time) ' s\n']);
+        printf(obj.log,'debug',['Total time: ' num2str(obj.display_para.solver_build_time+solveTime) ' s\n']);
+
+elseif feasibility_flag == -2
+
+
+        % print last iterate
+        % printf(obj.log,'debug','%-8d%-15e%-15e%-15e%-15e%-10f%-10e\n',...
+        %     iter, f_x_k , delta_xi_double, delta_dual_double, theta_x_k , alpha_k , full(casadi.DM( full(obj.eval_gradLang(x_k,p0,dual_k)) ) )   );
+        % 
+
+        printf(obj.log,'debug','------------------------------------------------------------------------------------------\n');
+        printf(obj.log,'debug','Solution status: Feasible solution. Restoration not invoked because already feasible.\n');
+        solveTime = toc(measTime_seqSOS_in);
+        printf(obj.log,'debug',['Solution time: ' num2str(solveTime) ' s\n']);
+        printf(obj.log,'debug',['Build time: ' num2str(obj.display_para.solver_build_time) ' s\n']);
+        printf(obj.log,'debug',['Total time: ' num2str(obj.display_para.solver_build_time+solveTime) ' s\n']); 
+end
+
+
+% assign output
+argout = sol;
+
+% store iteration info
+obj.info.iter = info(1:iter-1);
+
+end % end of function
