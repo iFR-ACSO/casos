f927761bb1e6fe9de68947eb4af855fa2d9c9fc8
JOlCode <61011693+JOlCode@users.noreply.github.com>
2025-01-28 09:43:04 +0100

    parameterize cholseky pre-computation; default value is compute it numerically online

--- +casos/+package/+solvers/@MosekInterface/eval.m
+++ +casos/+package/+solvers/@MosekInterface/eval.m
@@ -3,9 +3,17 @@ function argout = eval(obj,argin)
 
 msk_prob = obj.cone;
 
+args = cell2struct(argin',fieldnames(obj.args_in));
+
+% compute cholseky numerically
+if nnz(obj.args_in.h) > 0 && obj.opts.hessian_cholesky
+    args.h = chol((args.h));
+end
+
+
 % evaluate problem structure
-prob = call(obj.fhan,cell2struct(argin',fieldnames(obj.args_in)));
-data = call(obj.barv,cell2struct(argin',fieldnames(obj.args_in)));
+prob = call(obj.fhan,args);
+data = call(obj.barv,args);
 
 % to double
 msk_prob.a = sparse(prob.a);
