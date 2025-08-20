82ba488776cf52776cd55f25ce8739f8b5836d96
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2025-08-20 20:11:06 +0200

    Remove old operator implementation.

--- +casos/+package/+core/@AbstractOperator/Fnorm2.m
+++
@@ -1,6 +0,0 @@
-function r = Fnorm2(obj)
-% Compute the squared Frobenius norm of an operator.
-
-r = trace(obj.matrix'*obj.matrix);
-
-end
--- +casos/+package/+core/@AbstractOperator/str.m
+++
@@ -1,9 +0,0 @@
-function s = str(obj)
-% Return string representation.
-
-sp_in = to_char(obj.sparsity_in);
-sp_out = to_char(obj.sparsity_out);
-
-s = compose('Operator: [%s] -> [%s]',sp_in,sp_out);
-
-end
