670b10b9e8fb5022c32dd0372024cb1f8fbec1c4
Torbjoern Cunis <torbjoern.cunis@gmail.com>
2026-08-11 20:35:23 +0200

    Add reuse header.

--- +casos/+package/+core/@Polynomial/to_function.m
+++ +casos/+package/+core/@Polynomial/to_function.m
@@ -1,3 +1,9 @@
+% SPDX-FileCopyrightText: 2026 Institute of Flight Mechanics and Controls, University of Stuttgart
+% SPDX-FileCopyrightText: Author(s): Torbjørn Cunis <tcunis@ifr.uni-stuttgart.de>
+% SPDX-FileContributor: For a full list of contributors, see <https://github.com/ifr-ofc/casos>
+%
+% SPDX-License-Identifier: GPL-3.0-only
+
 function f = to_function(obj,variables,opts)
 % Return casadi.Function object using SX.
 
